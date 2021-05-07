ee2da8a8074df659b5623dc0c28c343f872fc1ce
Hao Zhang <zhisbug@gmail.com>
2021-05-07 01:32:59 -0400

    address circular dependency

--- parax/pipeline_parallel.py
+++ parax/pipeline_parallel.py
@@ -2,82 +2,21 @@
 import itertools as it
 from dataclasses import dataclass, field
 from typing import Sequence, List, Set, Any, Dict
-from abc import ABC, abstractmethod
-
-import numpy as np
 
 import jax
+from abc import ABC, abstractmethod
 from jax import jit
 from jax import linear_util as lu
-from jax.core import Atom, Var, DropVar, JaxprEqn, Jaxpr, ClosedJaxpr, Primitive, Literal, abstract_unit, jaxpr_as_fun
-from jax.interpreters import xla, ad, partial_eval as pe
-from jax.lib import xla_client as xc, xla_bridge as xb
 from jax._src.util import partial, safe_map, extend_name_stack, wrap_name
+from jax.core import Atom, Var, DropVar, JaxprEqn, Jaxpr, ClosedJaxpr, Literal, jaxpr_as_fun
+from jax.interpreters import partial_eval as pe
+from jax.lib import xla_bridge as xb
 
 unsafe_map, map = map, safe_map  # type: ignore
 
 from parax.pipeline_primitive_def import *
-# Note: import after the above lines
 from parax.pipe import JaxPipeline
-from parax.pipeline_stage import PipelineStage, PicklableStage
-
-
-# Define a Jax primitive to mark start/end of a pipeline stage.
-pipeline_p = Primitive('pipeline')
-pipeline_p.multiple_results = True
-
-def mark_pipeline(*args, name: str, mark_type: str):
-    """Mark the start/end of a pipeline stage.
-
-    Args:
-        *args: represents the pipeline input/output of a pipeline stage.
-        name (str): Name of the pipeline stage.
-        mark_type (str): start or end of a pipeline stage, can be "start",
-            "end", "jvp_start", or "jvp_end". The latter two are used for
-            backward pass.
-    """
-    if mark_type not in ('start', 'end', 'jvp_start', 'jvp_end'):
-        raise ValueError('Unknown mark type: %s' % mark_type)
-    return pipeline_p.bind(*args, name=name, mark_type=mark_type)
-
-def _pipeline_impl(*args, **kwargs):
-    # The pipeline marker acts as an identity function.
-    return args if len(args) > 0 else (None, )
-
-def _pipeline_abstract_eval(*args, **kwargs):
-    return args if len(args) > 0 else (abstract_unit, )
-
-def _pipeline_xla_translation(c, *args, **kwargs):
-    return xc.ops.Tuple(c, args) if len(args) > 0 else xc.ops.Tuple(c, (xc.ops.Constant(c, np.float32(0.0)), ))
-
-def _pipeline_value_and_jvp(arg_values, arg_tangents, name, mark_type):
-    primal_outs = mark_pipeline(*arg_values, name=name, mark_type=mark_type)
-    # TODO(zhuohan): Check the semantics here works for higher order gradients.
-    if mark_type == "start" or mark_type == "jvp_start":
-        tangent_mark_type = "jvp_start"
-    elif mark_type == "end" or mark_type == "jvp_end":
-        tangent_mark_type = "jvp_end"
-    else:
-        raise ValueError("Invalid mark_type")
-    tangent_outs = mark_pipeline(*arg_tangents, name=name, mark_type=tangent_mark_type)
-    return primal_outs, tangent_outs
-
-def _pipeline_transpose(ct, *args, name, mark_type):
-    # TODO(zhuohan): Check the semantics here works for higher order gradients.
-    if mark_type == "start" or mark_type == "jvp_start":
-        transposed_mark_type = "end"
-    elif mark_type == "end" or mark_type == "jvp_end":
-        transposed_mark_type = "start"
-    else:
-        raise ValueError("Invalid mark_type")
-    res = mark_pipeline(*ct, name=name, mark_type=transposed_mark_type)
-    return res
-
-pipeline_p.def_impl(_pipeline_impl)
-pipeline_p.def_abstract_eval(_pipeline_abstract_eval)
-xla.translations[pipeline_p] = _pipeline_xla_translation
-ad.primitive_jvps[pipeline_p] = _pipeline_value_and_jvp
-ad.primitive_transposes[pipeline_p] = _pipeline_transpose
+from parax.pipeline_stage import PipelineStage
 
 
 @dataclass
@@ -258,7 +197,8 @@ def slice_closed_jaxpr_by_pipeline_marks(closed_jaxpr: ClosedJaxpr) -> Sequence[
         assert current_stage is not None
 
         for var in eqn.invars:
-            if isinstance(var, Literal) or (var in current_stage.pipeline_invars) or (var in current_stage.intermediate_vars):
+            if isinstance(var, Literal) or (var in current_stage.pipeline_invars) or (
+                    var in current_stage.intermediate_vars):
                 continue
             elif var in global_consts_dir:
                 if var not in current_stage.consts_dir:
@@ -307,7 +247,7 @@ class LocalPipelineRunner:
         self.env = {}
         self.global_invals = global_invals
 
-    def run_stage(self, stage: PipelineStage, prev_stage_pipeline_outvals: Set[Var]=None):
+    def run_stage(self, stage: PipelineStage, prev_stage_pipeline_outvals: Set[Var] = None):
         """Run a pipeline stage.
 
         Args:
@@ -342,7 +282,8 @@ class LocalPipelineRunner:
         return pipeline_outvals, global_outvals
 
 
-def local_pipeline_runtime(pipeline_stages: Sequence[PipelineStage], global_invars: Sequence[Var], global_outvars: Sequence[Var]):
+def local_pipeline_runtime(pipeline_stages: Sequence[PipelineStage], global_invars: Sequence[Var],
+                           global_outvars: Sequence[Var]):
     """Return a callable that runs all pipeline stages on a single local device.
 
     Args:
@@ -351,6 +292,7 @@ def local_pipeline_runtime(pipeline_stages: Sequence[PipelineStage], global_inva
         global_invars (Sequence[Var]): Global input variables.
         global_outvars (Sequence[Var]): Global output variables.
     """
+
     def ret_func(*args, **kwargs):
         assert not kwargs, "kwargs not supported"
         global_invals = dict(zip(global_invars, args))
@@ -375,8 +317,8 @@ def local_pipeline_runtime(pipeline_stages: Sequence[PipelineStage], global_inva
 
 @lu.cache
 def pipeline_parallel_callable(
-    fun: lu.WrappedFun,
-    *avals
+        fun: lu.WrappedFun,
+        *avals
 ):
     with jax.disable_jit():
         jaxpr, out_avals, consts = pe.trace_to_jaxpr_final(fun, avals)
@@ -394,12 +336,12 @@ def pipeline_parallel_callable(
     xla_pipeline_stages = [XlaPipelineStage.from_jax_pipeline_stage(stage) for stage in jax_pipeline_stages]
     return local_pipeline_runtime(xla_pipeline_stages, global_invars, global_outvars)
 
+
 @lu.cache
 def distributed_pipeline_parallel_callable(
-    fun: lu.WrappedFun,
-    *avals
+        fun: lu.WrappedFun,
+        *avals
 ):
-
     with jax.disable_jit():
         jaxpr, out_avals, consts = pe.trace_to_jaxpr_final(fun, avals)
     closed_jaxpr = ClosedJaxpr(jaxpr, consts)

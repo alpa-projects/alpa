88dcddb6dc48ba9324a00e84f4e3f0074eb536e0
Zhuohan Li <zhuohan123@gmail.com>
2021-05-18 01:25:19 -0700

    check sizes

--- playground/pipeline_marker/kernel.cu
+++ playground/pipeline_marker/kernel.cu
@@ -3,6 +3,10 @@
 
 namespace kernel{
 void pipelineMarker(cudaStream_t stream, void **buffers, const char *opaque, size_t opaque_len) {
+    const int64_t *sizes = reinterpret_cast<const int64_t *> opaque;
+    for (size_t i = 0; i < opaque_len; i++) {
+        printf("%lld %lld\n", i, sizes[i])
+    }
     const float *x = reinterpret_cast<const float *>(buffers[0]);
     float *result = reinterpret_cast<float *>(buffers[1]);
     cudaMemset(result, 0, 1);

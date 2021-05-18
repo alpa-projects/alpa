a5301ce69335553d43a4ba4feca0b2cc830fff28
Zhuohan Li <zhuohan123@gmail.com>
2021-05-17 18:22:21 -0700

    test call pipeline_marker

--- playground/pipeline_marker/kernel.cu
+++ playground/pipeline_marker/kernel.cu
@@ -2,38 +2,9 @@
 #include<stdio.h>
 
 namespace kernel{
-
-void offloadToHost(cudaStream_t stream, void **buffers, const char *opaque, size_t opaque_len) {
+void pipelineMarker(cudaStream_t stream, void **buffers, const char *opaque, size_t opaque_len) {
     const float *x = reinterpret_cast<const float *>(buffers[0]);
-    float *key = reinterpret_cast<float *>(buffers[1]);
-    const swapHelper *s = reinterpret_cast<const swapHelper*>(opaque);
-    auto allocInfo = s->manager->allocMemory(s->TensorSize);
-
-    cudaMemcpy((float *)allocInfo.second,
-        x,
-        s->TensorSize,
-        cudaMemcpyDeviceToHost
-    );
-    cudaMemcpy((float *)key,
-        &allocInfo.first,
-        sizeof(float),
-        cudaMemcpyHostToDevice);
-}
-
-void uploadToDevice(cudaStream_t stream, void **buffers, const char *opaque, size_t opaque_len) {
-    const float *key = reinterpret_cast<const float*>(buffers[0]);
     float *result = reinterpret_cast<float *>(buffers[1]);
-    const swapHelper *s = reinterpret_cast<const swapHelper *>(opaque);
-    float host_key;
-    cudaMemcpy(&host_key, key, sizeof(float), cudaMemcpyDeviceToHost);
-
-    auto entry = s->manager->getMemInfo(host_key);
-
-    cudaMemcpy(result,
-        entry.first,
-        entry.second,
-        cudaMemcpyHostToDevice
-    );
+    cudaMemset(result, 0, 1);
 }
-
 };  //end namespace kernel
--- playground/pipeline_marker/test_pipeline_marker.py
+++ playground/pipeline_marker/test_pipeline_marker.py
@@ -7,6 +7,10 @@ from jax.lib import xla_client, xla_bridge
 
 ops = xla_client.ops
 
+import gpu_custom_call_test
+xla_client.register_custom_call_target(b'pipeline_marker',
+    gpu_custom_call_test.pipeline_marker(), platform='gpu')
+
 
 def test_simple_graph():
     c = xla_client.XlaBuilder("simple_graph")
@@ -17,6 +21,13 @@ def test_simple_graph():
 
     z = ops.Add(x, y)
     z = ops.Add(z, y)
+    out_shape = xla_client.Shape.array_shape(np.dtype(np.float32), (1,), (0,))
+    z = xla_client.ops.CustomCall(z,
+        b'pipeline_marker',
+        operands=(z,),
+        shape=out_shape,
+        opaque=b"abc"
+        )
 
     c = c.build(z)
     print("=" * 60)

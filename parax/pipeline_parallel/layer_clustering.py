db1a45c87cabe9a398377f35362386b570d471a4
Zhuohan Li <zhuohan123@gmail.com>
2021-08-30 21:12:32 -0700

    fix import

--- parax/pipeline_parallel/layer_clustering.py
+++ parax/pipeline_parallel/layer_clustering.py
@@ -10,10 +10,9 @@ from jax import tree_flatten
 from jax import lax
 from jax._src.api import make_jaxpr, _check_scalar
 from jax.lib import xla_client as xc, xla_bridge as xb
-from jax.core import JaxprEqn, Jaxpr, Var, jaxpr_as_fun
+from jax.core import ClosedJaxpr, JaxprEqn, Jaxpr, Var, Literal, DropVar, jaxpr_as_fun
 from jax.interpreters import xla
 
-from parax import mark_pipeline_jaxpreqn
 from .primitive_def import mark_pipeline
 
 # TODO: different operations takes different time

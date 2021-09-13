a757e49523bc97b1d90c94c6466f4df74bfb9ef3
Lianmin Zheng <lianminzheng@gmail.com>
2021-09-13 10:15:47 +0000

    update benchmark scripts

--- benchmark/parax/benchmark_transformer_layer_3d.py
+++ benchmark/parax/benchmark_transformer_layer_3d.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 import gc
 import os
 import time
@@ -13,7 +14,8 @@ import ray
 from parax import (parallelize, global_config, set_parallelize_options, testing,
                    DeviceCluster, PhysicalDeviceMesh, mark_pipeline)
 from parax.model.bert_model import BertConfig, FlaxBertAttention, FlaxBertLayerCollection
-from parax.util import run_cmd, write_tsv, benchmark_func, list_gpu_info
+from parax.util import (run_cmd, write_tsv, benchmark_func, list_gpu_info,
+                        count_communication_primitives)
 
 import timeit
 
@@ -47,14 +49,13 @@ def benchmark_transformer_one_case(benchmark_case, use_profiling):
     log_time_stamp(None)
 
     # Model configs
-    batch_size, seq_len, hidden_size, num_layers, num_heads, dp_size, \
-    tensor_mp_size, pipeline_mp_size, num_micro_batches, force_data_parallel \
+    batch_size, seq_len, hidden_size, num_layers, num_heads, mesh_dim0, \
+    mesh_dim1, pipeline_mp_size, num_micro_batches, force_data_parallel \
     = benchmark_case
     is_pipeline_parallel = True if pipeline_mp_size > 1 else False
     dtype = jnp.float32
-    if force_data_parallel:
-        global_config.force_batch_dim_to_mesh_dim = 0
-        global_config.allow_all_gather = False
+
+    global_config.force_data_parallel = force_data_parallel
 
     # Mesh configs
     if args.local:
@@ -64,7 +65,7 @@ def benchmark_transformer_one_case(benchmark_case, use_profiling):
         virtual_mesh = device_cluster.get_virtual_mesh()
         # virtual_mesh = virtual_mesh.slice(1, [[0, 1, 2, 3]])
     #     physical_mesh = device_cluster.get_physical_mesh()
-    # logical_mesh = physical_mesh.get_logical_mesh([dp_size, tensor_mp_size],
+    # logical_mesh = physical_mesh.get_logical_mesh([mesh_dim0, mesh_dim1],
     #                                               mesh_topology="tree",
     #                                               inter_host_bandwidth=1,
     #                                               intra_host_bandwidth=30)
@@ -85,9 +86,9 @@ def benchmark_transformer_one_case(benchmark_case, use_profiling):
     log_time_stamp("Setup device mesh")
 
     @parallelize
-    def train_step(optimizer, batch, apply_fn):
+    def train_step(optimizer, batch, rng_key, apply_fn):
         def loss_func(params, hidden_states):
-            rngs = {"dropout": batch["rng"]}
+            rngs = {"dropout": rng_key}
             if pipeline_mp_size:
                 hidden_states, = mark_pipeline(hidden_states, name="0", mark_type="start")
             out = apply_fn(params, hidden_states, batch["attention_mask"],
@@ -106,7 +107,6 @@ def benchmark_transformer_one_case(benchmark_case, use_profiling):
         "hidden_states": jnp.ones((batch_size, seq_len, hidden_size), dtype=np.float32),
         "attention_mask": jnp.ones((batch_size, seq_len), dtype=np.int32),
         "label": jnp.ones((batch_size, seq_len, hidden_size), dtype=np.float32),
-        "rng": jax.random.PRNGKey(0),
     }
     log_time_stamp("Prepare input")
 
@@ -120,58 +120,53 @@ def benchmark_transformer_one_case(benchmark_case, use_profiling):
     rngkey = jax.random.PRNGKey(0)
     params = model.init_dummy(rngkey, batch["hidden_states"], batch["attention_mask"])
     optimizer = optim.Adam(1e-2).create(params)
-    del (params, rngkey)
+    del params
     log_time_stamp("Init model and optimizer")
 
     # Compile executable
-    executable = train_step.get_executable(optimizer, batch, model.apply)
+    executable = train_step.get_executable(optimizer, batch, rngkey, model.apply)
     log_time_stamp("Compile (driver)")
 
     # Benchmark step time
     def run_func():
         nonlocal optimizer
-        # optimizer = train_step(optimizer, batch, model.apply)
-        train_step(optimizer, batch, model.apply)
+        # optimizer = train_step(optimizer, batch, rngkey, model.apply)
+        train_step(optimizer, batch, rngkey, model.apply)
 
-    def sync_func():
-        if not is_pipeline_parallel:
-            physical_mesh.sync_workers()
-        return
-
-
-    costs = benchmark_func(run_func, sync_func,
-                           warmup=1, repeat=2, number=args.number)
+    costs = benchmark_func(run_func, warmup=1, repeat=2, number=args.number)
     log_time_stamp("Benchmark")
 
     # Check sharding strategy
     if not is_pipeline_parallel:
-        real_mem = physical_mesh.get_total_allocation_size(executable)
         objective = testing.last_compiled_auto_sharding_objective or 0.0
-        hlo_module = testing.last_compiled_executable.hlo_modules()[0]
-        hlo_ir = hlo_module.to_string()
-        print(f" - #comm {hlo_ir.count('channel_id')}, " +
-              f"#all-reduce {hlo_ir.count('all-reduce(') + hlo_ir.count('all-reduce-start(')}")
+        real_mem = executable.get_total_allocation_size()
+        hlo_text = executable.get_hlo_text()
 
         with open("last.hlo", "w") as fout:
-            fout.write(hlo_ir)
+            fout.write(hlo_text)
+        n_total, n_all_reduce, n_all_gather, n_reduce_scatter, _ =\
+            count_communication_primitives(hlo_text)
+        print(f"#total: {n_total}, #all-reduce: {n_all_reduce}, "
+              f"#all-gather: {n_all_gather}, #reduce-scatter: {n_reduce_scatter}")
     else:
         real_mem = -1
         objective = -1
 
     # Log benchmark results
-    heads = ["Type", "Case", "Mesh Shape", "Peak Mem", "Objective", "Mean Time", "Std Time"]
+    heads = ["Type", "Model Config", "Parallel Config", "Peak Mem",
+             "Objective", "Mean Time", "Std Time"]
     values = ["transformer-layer", str(benchmark_case[:-5]), str(benchmark_case[-5:]),
-              f"{real_mem/GB:.3f}", f"{objective:.2f}",
-              f"{np.mean(costs):.3f}", f"{np.std(costs):.3f}"]
+             f"{real_mem/GB:.3f}", f"{objective:.2f}",
+             f"{np.mean(costs):.3f}", f"{np.std(costs):.3f}"]
     write_tsv(heads, values, "result_trans.tsv")
 
     # physical_mesh.shutdown()
 
 # B = batch_size, S = seq_len, H = hidden_size, L = num_layers,
-# #head = num_heads, D1 = mesh_dimension_1, D2 = mesh_dimension_2
+# #head = num_heads, D0 = mesh_dimension_0, D1 = mesh_dimension_1
 
 benchmark_suite_4_gpu = [
-    # # B,  S,    H,    L,  #head,     D1, D2, PP, NB, FD
+    # # B,  S,    H,    L,  #head,     D0, D1, PP, NB, FD
     (32,  1024, 1536, 2,  1536//96,  1,  2, 2, 1, False),
     (32,  1024, 1536, 2,  1536//96,  2,  1, 2, 1, False),
     (32,  128,  5120, 2,  5120//128, 1,  2, 2, 1, False),
@@ -185,9 +180,9 @@ benchmark_suite_4_gpu = [
 ]
 
 benchmark_suite_8_gpu = [
-    # B,  S,    H,    L,  #head,     D1, D2, PP, NB, FD
+    # B,  S,    H,    L,  #head,     D0, D1, PP, NB, FD
     # (32,  1024, 1536, 2,  1536//96,  4,  1, 2, 1, False),
-    (16,  1024, 1536, 2,  1536//96,  4,  1, 2, 1, False),
+    (16,  1024, 1536, 2,  1536//96,  4,  1, 2, 1, True),
     #
     # (32,  128,  5120, 2,  5120//128, 1,  4, 2, 1, False),
     # (32,  128,  5120, 2,  5120//128, 4,  1, 2, 1, False),
@@ -206,7 +201,13 @@ def benchmark_all(use_profiling):
     }
 
     for case in benchmark_suites[num_gpus]:
+        # Backup global config
+        old_global_config = copy.deepcopy(global_config.__dict__)
+
         benchmark_transformer_one_case(case, use_profiling)
+
+        # Restore global config
+        global_config.__dict__ = old_global_config
 
 
 if __name__ == "__main__":

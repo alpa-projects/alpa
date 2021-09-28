114f103f058ce92e4ec53eb5263118212e3641d7
Lianmin Zheng <lianminzheng@gmail.com>
2021-09-28 12:52:50 +0000

    fix pipeline bert test

--- tests/test_pipeline_bert.py
+++ tests/test_pipeline_bert.py
@@ -74,10 +74,10 @@ class PipelineBERTTest(unittest.TestCase):
             # new_optimizer = optimizer.apply_gradient(grad_param)
             return grad_param
 
-        batch_size = 4
-        seq_len = 64
-        hidden_size = 256
-        num_heads = 4
+        batch_size = 16
+        seq_len = 8
+        hidden_size = 512
+        num_heads = 8
 
         x = jnp.ones((batch_size, seq_len, hidden_size), dtype=jnp.float32)
         y = jnp.ones(

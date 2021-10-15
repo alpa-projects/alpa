d36d324a21c2c579c7b8b41551387a5a11c39399
ZYHowell <zhuangyh@sjtu.edu.cn>
2021-10-15 22:03:06 +0000

    not donate global vars used simultaneously

--- tests/test_pipeline_gradient_accumulation.py
+++ tests/test_pipeline_gradient_accumulation.py
@@ -1,6 +1,7 @@
 import unittest
 
-from flax import linen as nn, optim
+from flax import linen as nn
+import optax
 import jax
 import jax.numpy as jnp
 import numpy as np
@@ -9,9 +10,20 @@ import ray
 import parax
 from parax import (parallelize, global_config, set_parallelize_options,
                    DeviceCluster, manual_layer_slicing)
-from parax.model.bert_model import BertConfig, FlaxBertLayer
+from parax.model.bert_model import BertConfig, FlaxBertLayer, TrainState
 from parax.pipeline_parallel.primitive_def import mark_pipeline
 from parax.testing import assert_allclose
+
+
+def create_train_state(rngkey, model, params):
+    params = model.init_dummy(rngkey, *params)
+    tx = optax.adam(learning_rate=1e-2)
+    state = TrainState.create(
+        apply_fn=model.apply,
+        params=params,
+        tx=tx,
+        dynamic_scale=None)
+    return state
 
 
 class MLP_Model(nn.Module):
@@ -22,11 +34,11 @@ class MLP_Model(nn.Module):
     def __call__(self, x):
         mark_pipeline(name='1', mark_type='start')
         x = nn.Dense(features=self.hidden_dim, use_bias=True)(x)
-        x = nn.relu(x)
-        x = nn.Dense(features=self.hidden_dim, use_bias=True)(x)
+        # x = nn.relu(x)
+        # x = nn.Dense(features=self.hidden_dim, use_bias=True)(x)
         mark_pipeline(name='1', mark_type='end')
         mark_pipeline(name='2', mark_type='start')
-        x = nn.Dense(features=self.hidden_dim, use_bias=True)(x)
+        # x = nn.Dense(features=self.hidden_dim, use_bias=True)(x)
         x = nn.Dense(features=self.output_dim, use_bias=True)(x)
         return x
 
@@ -69,9 +81,8 @@ class AccumulateGradTest(unittest.TestCase):
         x = jnp.array(np.random.rand(batch_size, input_dim))
         y = jnp.array(np.random.rand(batch_size, output_dim))
         rngkey = jax.random.PRNGKey(0)
-        params = model.init(rngkey, x)
-        optimizer = optim.GradientDescent(1e-2).create(params)
         batch = {'x': x, 'y': y}
+        state = create_train_state(rngkey, model, [x])
 
         @manual_layer_slicing
         def loss_func(params, x, y):
@@ -80,24 +91,26 @@ class AccumulateGradTest(unittest.TestCase):
             mark_pipeline(name='2', mark_type='end')
             return loss
 
-        def train_step(optimizer, batch):
-            param_grad = parax.grad(loss_func)(optimizer.target, batch['x'],
+        def train_step(state, batch):
+            param_grad = parax.grad(loss_func)(state.params, batch['x'],
                                                batch['y'])
-            new_optimizer = optimizer.apply_gradient(param_grad)
-            return new_optimizer.target
+            new_optimizer = state.apply_gradients(grads=param_grad)
+            return new_optimizer
 
         global_config.num_micro_batches = 4
 
         # copy to prevent from donation
-        corr = train_step(optimizer, batch)
+        corr = train_step(state, batch)
         parallel_train_step = parallelize(train_step)
-        new_optimizer = parallel_train_step(optimizer, batch)
-        assert_allclose(new_optimizer, corr)
-        parallel_train_step.get_executable(optimizer, batch).shutdown()
+        from jax import tree_flatten
+        print(tree_flatten(state)[1])
+        new_optimizer = parallel_train_step(state, batch)
+        assert_allclose(new_optimizer.params, corr.params)
+        parallel_train_step.get_executable(state, batch).shutdown()
 
     def test_2_layer_bert(self):
 
-        def train_step(optimizer, batch, apply_fn):
+        def train_step(state, batch, apply_fn):
 
             @manual_layer_slicing
             def loss_func(params, x, y, attention_mask):
@@ -106,12 +119,12 @@ class AccumulateGradTest(unittest.TestCase):
                 mark_pipeline(name='2', mark_type='end')
                 return loss
 
-            grad_param = parax.grad(loss_func)(optimizer.target, batch['x'],
+            grad_param = parax.grad(loss_func)(state.params, batch['x'],
                                                batch['y'],
                                                batch['attention_mask'])
 
-            new_optimizer = optimizer.apply_gradient(grad_param)
-            return new_optimizer.target
+            new_optimizer = state.apply_gradients(grads=grad_param)
+            return new_optimizer
 
         batch_size = 16
         seq_len = 8
@@ -130,17 +143,16 @@ class AccumulateGradTest(unittest.TestCase):
                               intermediate_size=hidden_size * 4,
                               num_attention_heads=num_heads))
         rngkey = jax.random.PRNGKey(0)
-        params = model.init(rngkey, x, attention_mask)
-        optimizer = optim.GradientDescent(1e-2).create(params)
         batch = {"x": x, "y": y, "attention_mask": attention_mask}
+        state = create_train_state(rngkey, model, [x, attention_mask])
 
         global_config.num_micro_batches = 2
 
-        corr_tgt = train_step(optimizer, batch, model.apply)
+        corr_tgt = train_step(state, batch, model.apply)
         pipelined_train_step = parallelize(train_step)
-        pipe_tgt = pipelined_train_step(optimizer, batch, model.apply)
-        assert_allclose(corr_tgt, pipe_tgt)
-        pipelined_train_step.get_executable(optimizer, batch, model.apply).shutdown()
+        pipe_tgt = pipelined_train_step(state, batch, model.apply)
+        assert_allclose(corr_tgt.params, pipe_tgt.params)
+        pipelined_train_step.get_executable(state, batch, model.apply).shutdown()
 
 
 def suite():

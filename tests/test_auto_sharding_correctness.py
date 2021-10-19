f8cb7fe4992d4e6c0697a6886ab06f2eed5b6c46
Hao Zhang <zhisbug@gmail.com>
2021-10-18 20:58:24 -0700

    some minor updates

--- tests/test_auto_sharding_correctness.py
+++ tests/test_auto_sharding_correctness.py
@@ -46,6 +46,7 @@ class AccumulateGradTest(unittest.TestCase):
         jax.config.update('jax_platform_name', 'cpu')
         self.physical_mesh = DeviceCluster().get_physical_mesh()
         logical_mesh = self.physical_mesh.get_logical_mesh([2, 2], [2, 2], [1, 0.1])
+        self.dtype = jnp.float32
         set_parallelize_options(logical_mesh)
 
     def tearDown(self):
@@ -55,7 +56,6 @@ class AccumulateGradTest(unittest.TestCase):
 
         def train_step(state, batch, apply_fn):
 
-            # @manual_layer_slicing
             def loss_func(params, x, y, attention_mask):
                 out = apply_fn(params, x, attention_mask)
                 loss = jnp.mean((out - y)**2)
@@ -64,7 +64,6 @@ class AccumulateGradTest(unittest.TestCase):
             grad_param = parax.grad(loss_func)(state.params, batch['x'],
                                                batch['y'],
                                                batch['attention_mask'])
-
             new_state = state.apply_gradients(grads=grad_param)
             return new_state, grad_param
 
@@ -73,17 +72,18 @@ class AccumulateGradTest(unittest.TestCase):
         hidden_size = 512
         num_heads = 8
 
-        x = jnp.ones((batch_size, seq_len, hidden_size), dtype=jnp.float32)
+        x = jnp.ones((batch_size, seq_len, hidden_size), dtype=self.dtype)
         y = jnp.ones(
             (batch_size, seq_len, hidden_size),
-            dtype=jnp.float32) * 23  # * np.arange(hidden_size)[None, None, :]
-        attention_mask = jnp.ones((batch_size, seq_len), dtype=jnp.float32)
+            dtype=self.dtype)  # * np.arange(hidden_size)[None, None, :]
+        attention_mask = jnp.ones((batch_size, seq_len), dtype=self.dtype)
 
         # Init model and optimizer
         model = BertLayer_Model(
             config=BertConfig(hidden_size=hidden_size,
                               intermediate_size=hidden_size * 4,
-                              num_attention_heads=num_heads))
+                              num_attention_heads=num_heads),
+            dtype=self.dtype)
         rngkey = jax.random.PRNGKey(0)
         batch = {"x": x, "y": y, "attention_mask": attention_mask}
         state = create_train_state(rngkey, model, [x, attention_mask])
@@ -93,10 +93,18 @@ class AccumulateGradTest(unittest.TestCase):
         parallel_train_step = parallelize(train_step)
         expected_params, expected_grads = train_step(state, batch, model.apply)
         actual_params, actual_grads = parallel_train_step(state, batch, model.apply)
+
+        print("group 1:")
         print("expected param example: ", jax.tree_util.tree_flatten(expected_params.params)[0][0][0:10])
         print("actual param example: ", jax.tree_util.tree_flatten(actual_params.params)[0][0]._value[0:10])
         print("expected grad example: ", jax.tree_util.tree_flatten(expected_grads)[0][0][0:10])
         print("actual grad example: ", jax.tree_util.tree_flatten(actual_grads)[0][0]._value[0:10])
+
+        print("group 2:")
+        print("expected param example: ", jax.tree_util.tree_flatten(expected_params.params)[0][-1][0:100])
+        print("actual param example: ", jax.tree_util.tree_flatten(actual_params.params)[0][-1]._value[0:100])
+        print("expected grad example: ", jax.tree_util.tree_flatten(expected_grads)[0][-1][0:100])
+        print("actual grad example: ", jax.tree_util.tree_flatten(actual_grads)[0][-1]._value[0:100])
 
         assert_allclose(expected_params.params, actual_params.params)
         self.physical_mesh.shutdown()

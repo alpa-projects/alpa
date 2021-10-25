2b3e5a34a0882cc8682c355c6794c4873e33c20f
Lianmin Zheng <lianminzheng@gmail.com>
2021-10-25 09:02:40 +0000

    rewrite mixed precision training

--- benchmark/parax/benchmark_gpt_bert.py
+++ benchmark/parax/benchmark_gpt_bert.py
@@ -76,10 +76,9 @@ def load_profiling_result(physical_mesh):
         physical_mesh.save_profiling_result(filename)
 
 
-def create_train_state(rngkey, model, batch):
+def create_train_state(rngkey, model, batch, dtype):
     params = model.init_dummy(rngkey, batch["input_ids"], batch["attention_mask"],
                               batch["token_type_ids"], batch["position_ids"])
-
     def weight_decay_mask(pytree):
         # do not use weight decay on layer norm and bias.
         return jax.tree_map(lambda x: x.ndim > 1, pytree)
@@ -89,10 +88,13 @@ def create_train_state(rngkey, model, batch):
         optax.adamw(learning_rate=1e-2, mask=weight_decay_mask)
     )
 
+    mixed_precision = (dtype == jnp.float16)
+
     state = TrainState.create(
         apply_fn=model.apply,
         params=params,
         tx=tx,
+        mixed_precision=mixed_precision,
         dynamic_scale=None)
     return state
  
@@ -117,8 +119,7 @@ def get_train_step(grad_func, num_layers, use_remat, dtype):
             loss = (label_mask * loss).sum() / label_mask.sum()
             return loss
 
-        params = jax.tree_util.tree_map(lambda x : jnp.asarray(x, dtype), state.params)
-        grads = grad_func(loss_func)(params)
+        grads = grad_func(loss_func)(state.params)
         new_state = state.apply_gradients(grads=grads)
         # TODO(lmzheng): add dynamic scale for mixed-precision training
         return new_state
@@ -191,7 +192,7 @@ def benchmark_gpt_bert_internal(physical_mesh, model_type, benchmark_case, niter
         raise ValueError(f"Invalid model {model_type}")
 
     rngkey = jax.random.PRNGKey(0)
-    state = create_train_state(rngkey, model, batch)
+    state = create_train_state(rngkey, model, batch, dtype)
     print_used_time("Create train state")
 
     # Compile executable
@@ -280,10 +281,11 @@ default_benchmark_suite = {  # key = number of gpus, value = a list of cases
 
 8: [
     # B,   S,    H,    L,  #head,     V,     D0, D1, NB, FD,    RS,    CK
-    (256, 512,  1024, 10, 1024//64,  25600, 8,  1,  1,  False,  True,  False),
-    (8,   1024, 4096, 10, 4096//128, 25600, 8,  1,  1,  True,   True,  False),
-    (8,   1024, 4096, 10, 4096//128, 25600, 2,  4,  1,  False,  True,  False),
-    (8,   1024, 4096, 10, 4096//128, 25600, 1,  8,  1,  False,  True,  False),
+    (256,  512,  1024, 10, 1024//64,  25600, 8,  1,  1,  False, True,  False),
+    (512,  512,  1024, 10, 1024//64,  25600, 8,  1,  2,  False, True,  False),
+    (8,    1024, 4096, 10, 4096//128, 25600, 8,  1,  1,  True,  True,  False),
+    (8,    1024, 4096, 10, 4096//128, 25600, 2,  4,  1,  False, True,  False),
+    (8,    1024, 4096, 10, 4096//128, 25600, 1,  8,  1,  False, True,  False),
 ],
 
 16: [

move mixed precision training to TrainState & fix reduce-scatter test cases

benchmark/parax/benchmark_moe.py
@@ -22,7 +22,7 @@ from benchmark_gpt_bert import load_profiling_result, get_train_step
 GB = 1024 ** 3
 
 
-def create_train_state(rngkey, model, batch):
+def create_train_state(rngkey, model, dtype, batch):
     params = model.init_dummy(rngkey, batch["input_ids"], batch["attention_mask"],
                               batch["token_type_ids"], batch["position_ids"])
 
@@ -34,10 +34,13 @@ def create_train_state(rngkey, model, batch):
         learning_rate=1e-2, weight_decay_mask=weight_decay_mask
     )
 
+    mixed_precision = (dtype == jnp.float16)
+
     state = TrainState.create(
         apply_fn=model.apply,
         params=params,
         tx=tx,
+        mixed_precision = (dtype == jnp.float16),
         dynamic_scale=None)
     return state
 
@@ -94,7 +97,7 @@ def benchmark_moe_internal(physical_mesh, benchmark_case, niter):
     ), dtype=dtype)
 
     rngkey = jax.random.PRNGKey(0)
-    state = create_train_state(rngkey, model, batch)
+    state = create_train_state(rngkey, model, dtype, batch)
     param_count = compute_param_number(state.params)
     print_used_time("Create train state")
 
@@ -177,15 +180,15 @@ default_benchmark_suite = {  # key = number of gpus, value = a list of cases
     (8,   1024, 1024, 12, 1024//64,  25600, 1024, 4,  1,  1,  1,  False, True,  False),
 ],
 
-4: [
+2: [
     #B,   S,    H,    L,  #head,     V,     S_,   E,  D0, D1, NB, FD,    RS,    CK
-    (16,  1024, 1792, 10, 1792//128, 25600, 1024, 8,  1,  4,  1,  True,  False, False),
+    (16,  1024, 1280, 12, 1280//128, 25600, 1024, 16, 1,  2,  1,  True,  True,  False),
 ],
 
 8: [
     #B,   S,    H,    L,  #head,     V,     S_,   E,  D0, D1, NB, FD,    RS,    CK
-    (16,  1024, 2560, 12, 2560//128, 25600, 1024, 16, 1,  8,  1,  False, True, False),
-    (16,  1024, 2560, 12, 2560//128, 25600, 1024, 16, 1,  8,  1,  True,  True, False),
+    (16,  1024, 2560, 12, 2560//128, 25600, 1024, 16, 1,  8,  1,  False, False, False),
+    (16,  1024, 2560, 12, 2560//128, 25600, 1024, 16, 1,  8,  1,  True,  True,  False),
 ],
 
 16: [

tests/test_auto_sharding_correctness.py
@@ -15,11 +15,10 @@ from parax.testing import assert_allclose
 def create_train_state(rngkey, model, batch_args):
     params = model.init(rngkey, *batch_args)
     tx = optax.adam(learning_rate=1e-2)
-    state = TrainState.create(
-        apply_fn=model.apply,
-        params=params,
-        tx=tx,
-        dynamic_scale=None)
+    state = TrainState.create(apply_fn=model.apply,
+                              params=params,
+                              tx=tx,
+                              dynamic_scale=None)
     return state
 
 
@@ -48,7 +47,8 @@ class AutoShardingCorrectnessTest(unittest.TestCase):
         def train_step(state, batch):
 
             def loss_func(params):
-                out = state.apply_fn(params, batch["x"], batch["attention_mask"])
+                out = state.apply_fn(params, batch["x"],
+                                     batch["attention_mask"])
                 loss = jnp.mean((out - batch["y"])**2)
                 return loss
 
@@ -63,16 +63,18 @@ class AutoShardingCorrectnessTest(unittest.TestCase):
         dtype = jnp.float32
 
         rngkey = jax.random.PRNGKey(0)
-        x = jax.random.normal(rngkey, (batch_size, seq_len, hidden_size), dtype=dtype)
-        y = jax.random.normal(rngkey, (batch_size, seq_len, hidden_size), dtype=dtype)
+        x = jax.random.normal(rngkey, (batch_size, seq_len, hidden_size),
+                              dtype=dtype)
+        y = jax.random.normal(rngkey, (batch_size, seq_len, hidden_size),
+                              dtype=dtype)
         attention_mask = jnp.ones((batch_size, seq_len), dtype=dtype)
 
         # Init model and optimizer
-        model = BertLayerModel(
-            config=BertConfig(hidden_size=hidden_size,
-                              intermediate_size=hidden_size * 4,
-                              num_attention_heads=num_heads),
-            dtype=dtype)
+        model = BertLayerModel(config=BertConfig(hidden_size=hidden_size,
+                                                 intermediate_size=hidden_size *
+                                                 4,
+                                                 num_attention_heads=num_heads),
+                               dtype=dtype)
         batch = {"x": x, "y": y, "attention_mask": attention_mask}
         state = create_train_state(rngkey, model, [x, attention_mask])
 
@@ -93,13 +95,17 @@ class AutoShardingCorrectnessTest(unittest.TestCase):
         # print("expected grad example: ", jax.tree_util.tree_flatten(expected_grads)[0][-1][0:100])
         # print("actual grad example: ", jax.tree_util.tree_flatten(actual_grads)[0][-1]._value[0:100])
 
-        assert_allclose(expected_params.params, actual_params.params, rtol=5e-4, atol=5e-4)
+        assert_allclose(expected_params.params,
+                        actual_params.params,
+                        rtol=5e-4,
+                        atol=5e-4)
         physical_mesh.shutdown()
 
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(AutoShardingCorrectnessTest("test_2_layer_bert_shard_parallel"))
+    suite.addTest(
+        AutoShardingCorrectnessTest("test_2_layer_bert_shard_parallel"))
     return suite
 
 
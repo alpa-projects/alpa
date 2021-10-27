eb2368e87ecc5b141789238ff9c6ee8086b5c4eb
Zhuohan Li <zhuohan123@gmail.com>
2021-10-26 23:01:34 -0700

    fix bug

--- tests/test_pipeline_gradient_accumulation.py
+++ tests/test_pipeline_gradient_accumulation.py
@@ -175,7 +175,8 @@ class AccumulateGradTest(unittest.TestCase):
         model = BertLayer_Model(
             config=BertConfig(hidden_size=hidden_size,
                               intermediate_size=hidden_size * 4,
-                              num_attention_heads=num_heads))
+                              num_attention_heads=num_heads),
+            manual_pipeline_layer=manual_pipeline_layer)
         batch = {"x": x, "y": y, "attention_mask": attention_mask}
         state = create_train_state(rngkey, model, [x, attention_mask])
 

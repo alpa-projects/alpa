5537e5e2816ce6291b2289d32864e960c284d6e3
Hao Zhang <zhisbug@gmail.com>
2021-11-10 00:01:50 -0800

    moe benchmark suites

--- parax/pipeline_parallel/three_d_parallel.py
+++ parax/pipeline_parallel/three_d_parallel.py
@@ -169,7 +169,16 @@ def three_d_parallel_callable(fun: lu.WrappedFun, in_tree, out_tree_thunk,
 
     # Wrap all things into a distributed runtime
     grad_in_to_out = {k: repr(v) for k, v in grad_in_to_out.items()}
-    jp = DecentralizedDistributedRuntime(pipeline_stages=xla_stages,
+    # jp = DecentralizedDistributedRuntime(pipeline_stages=xla_stages,
+    #                                      global_invars=global_invars,
+    #                                      grad_dummy_invars=grad_in_to_out,
+    #                                      global_outvars=global_outvars,
+    #                                      physical_meshes=physical_meshes,
+    #                                      dependency=dependency,
+    #                                      schedule=schedule,
+    #                                      is_batch=batch_invars,
+    #                                      num_batch=num_micro_batches)
+    jp = CentralizedDistributedRuntime(pipeline_stages=xla_stages,
                                          global_invars=global_invars,
                                          grad_dummy_invars=grad_in_to_out,
                                          global_outvars=global_outvars,

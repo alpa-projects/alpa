278250584f9cbb45e17e53a8657fd0d3ae12cb63
Hao Zhang <zhisbug@gmail.com>
2021-11-22 01:04:57 -0800

    add a kill script

--- benchmark/scripts/kill.sh
+++ benchmark/scripts/kill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/berkeley-aws-oregon.pem ubuntu@172.31.44.28 "killall python" &
-ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/berkeley-aws-oregon.pem ubuntu@172.31.38.88 "killall python" &
-ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/berkeley-aws-oregon.pem ubuntu@172.31.43.214 "killall python" &
+ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/berkeley-aws-oregon.pem ubuntu@172.31.44.28 "killall python3" &
+ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/berkeley-aws-oregon.pem ubuntu@172.31.38.88 "killall python3" &
+ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/berkeley-aws-oregon.pem ubuntu@172.31.43.214 "killall python3" &
 ps aux | grep python | grep -v "grep python" | awk '{print $2}' | xargs sudo kill -9

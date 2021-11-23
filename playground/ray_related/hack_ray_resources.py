2df6f44c141aff8da06a7c454e609fefab05d8d2
Zhuohan Li <zhuohan123@vip.qq.com>
2021-11-23 07:53:44 +0000

    fix more stuff

--- playground/ray_related/hack_ray_resources.py
+++ playground/ray_related/hack_ray_resources.py
@@ -1,15 +1,25 @@
 import ray
+import os
 import jax
 
-@ray.remote
+@ray.remote(runtime_env={
+    "env_vars": {
+        "CUDA_VISIBLE_DEVICS": "0,1,2,3,4,5,6,7",
+        "CUDA_VISIBLE_DEVICESS": "abcd"
+    }
+})
 class Actor:
     def __init__(self, name):
         self.name = name
-        print(self.name, jax.devices())
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4,5,6,7"
+        print(self.name, jax.devices(), os.getenv("CUDA_VISIBLE_DEVICES"), os.getenv("CUDA_VISIBLE_DEVICESS"))
+
+    def get_name(self):
+        return self.name
 
 ray.init("auto")
 
 actor_1 = Actor.remote("actor_1")
 actor_2 = Actor.remote("actor_2")
 
-print(ray.get([Actor.remote("A"), Actor.remote("B")]))
+ray.get([actor_1.get_name.remote(), actor_2.get_name.remote()])

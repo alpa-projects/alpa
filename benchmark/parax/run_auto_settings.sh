26afe89caf93cfdfbae4bccabc76c2b145951752
Zhuohan Li <zhuohan123@gmail.com>
2021-12-01 00:03:08 -0800

    fix name

--- benchmark/parax/run_auto_settings.sh
+++ benchmark/parax/run_auto_settings.sh
@@ -6,6 +6,6 @@ do
   ray stop
   ray start --head --num-gpus ${i}
   sleep 1
-  python benchmark_gpt_bert_3d.py --suite auto_gpt &> auto_gpt_${i}_gpus.log
+  python benchmark_gpt_bert_3d.py --suite auto_gpt --exp_name auto_${i}_gpus &> auto_gpt_${i}_gpus.log
   sleep 1
 done

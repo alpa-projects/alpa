fb31e0faa67544627c7278abb760bf5859a4b6f4
Yonghao Zhuang <zhuangyh@sjtu.edu.cn>
2021-12-07 02:24:41 -0800

    hot fix prune and benchmark (#248)

--- parax/pipeline_parallel/stage_construction.py
+++ parax/pipeline_parallel/stage_construction.py
@@ -228,7 +228,8 @@ def distributed_profile_on_mesh(meshes: Sequence[VirtualPhysicalMesh], layers,
         for end in tqdm.tqdm(range(start, num_layers), leave=False):
             if is_full_mesh and not (start == 0 and end == num_layers - 1):
                 continue
-            flops_ratio = (tot_flops[end + 1] - tot_flops[start]) / tot_flops
+            flops_ratio = (layer_flops_prefix_sum[end + 1] -
+                           layer_flops_prefix_sum[start]) / tot_flops
             if ((computation_source_ratio > flops_ratio * (1 + tolerance)) or
                 (computation_source_ratio < flops_ratio / (1 + tolerance))):
                 continue

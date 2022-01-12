158e2cdd8a2b56653e130bbdd94b8b480687a345
ZYHowell <zhuangyh@sjtu.edu.cn>
2022-01-12 03:58:12 +0000

    update test generate stage config

--- tests/test_pipeline_stage_construct_util.py
+++ tests/test_pipeline_stage_construct_util.py
@@ -3,7 +3,7 @@ import os
 
 import jax
 from jax._src.api import make_jaxpr
-from jax.core import ClosedJaxpr, gensym
+from jax.core import ClosedJaxpr, Var, gensym
 import jax.numpy as jnp
 from parax.model.bert_model import BertConfig
 from parax.pipeline_parallel.apply_grad import (
@@ -17,7 +17,7 @@ import ray
 
 from parax import DeviceCluster
 from parax.pipeline_parallel.stage_profiling import (
-    ApplyGradConfig, CompileConfig, generate_stage_info, compile_all,
+    ApplyGradConfig, CompileConfig, ProfileConfig, generate_stage_info, compile_all,
     profile_all, compute_intermediate_size, compute_apply_grad_invar_size)
 from parax.util import get_ray_namespace_str, OrderedSet
 from parax.testing import (BertLayerModel, create_train_state,
@@ -26,6 +26,7 @@ from parax.testing import (BertLayerModel, create_train_state,
 
 def _aval_key(a):
     return (a.shape, repr(a.dtype))
+
 
 class StageConstructUtilTest(unittest.TestCase):
 
@@ -101,7 +102,7 @@ class StageConstructUtilTest(unittest.TestCase):
 
         global_invars = closed_jaxpr.jaxpr.invars
         global_outvars = closed_jaxpr.jaxpr.outvars
-        donation_mapping = dict(grad_in_to_out) if have_apply_grad else {}
+        donation_mapping = dict(grad_in_to_out)
 
         num_forward_layers = len(jax_pipeline_layers) // 2
         layer_to_dummy_mesh = (list(range(num_forward_layers)) +
@@ -121,6 +122,7 @@ class StageConstructUtilTest(unittest.TestCase):
                 jax_apply_layers, apply_grad_donation, global_outvars)
 
     def _assert_avals_allmatch(self, aval_seq_a, aval_seq_b):
+        assert len(aval_seq_a) == len(aval_seq_b), f"{len(aval_seq_a)} != {len(aval_seq_b)}"
         aval_seq_a = sorted(aval_seq_a, key=_aval_key)
         aval_seq_b = sorted(aval_seq_b, key=_aval_key)
         for a, b in zip(aval_seq_a, aval_seq_b):
@@ -142,7 +144,28 @@ class StageConstructUtilTest(unittest.TestCase):
         intermediate_vars, stage_config = generate_stage_info(
             compute_layers, compute_layer_indices, donation_mapping,
             compute_outvars, "tmp", 0, apply_grad_config)
-        # check intermediate vars
+        aval = lambda x: [v.aval for v in x]
+        # check intermediate vars. It is only to compute intermediate size, so
+        # only check aval
+        backward_input = OrderedSet()
+        for idx in indices:
+            backward_idx = len(compute_layers) - idx - 1
+            active_vars = OrderedSet()
+            for eqn in compute_layers[backward_idx].eqns[1:]:
+                active_vars.update(
+                    [v for v in eqn.invars if isinstance(v, Var)])
+            start_marker = compute_layers[backward_idx].eqns[0]
+            active_vars.intersection_update(start_marker.outvars)
+            active_invars = OrderedSet()
+            for invar, outvar in zip(start_marker.invars, start_marker.outvars):
+                if outvar in active_vars:
+                    active_invars.add(invar)
+            backward_input.update(active_invars)
+        forward_output = OrderedSet()
+        for idx in indices:
+            forward_output.update(compute_layers[idx].outvars)
+        intermediate = forward_output.intersection(backward_input)
+        self._assert_avals_allmatch(aval(intermediate_vars), aval(intermediate))
         # check apply grad config
         config: ApplyGradConfig = stage_config.apply_grad_config
         apply_grad_invars = set()
@@ -150,6 +173,7 @@ class StageConstructUtilTest(unittest.TestCase):
             apply_grad_invars.update(layer.invars)
         assert apply_grad_invars.issubset(config.invars)
         assert apply_grad_invars.issuperset(config.invars)
+
         apply_grad_only_invars = set(apply_grad_invars)
         for i in compute_layer_indices:
             apply_grad_only_invars.difference_update(compute_layers[i].invars)
@@ -166,17 +190,67 @@ class StageConstructUtilTest(unittest.TestCase):
         input_vars.update(apply_grad_only_invars)
         input_avals = [var.aval for var in input_vars]
         self._assert_avals_allmatch(input_avals, config.input_avals)
-        config.output_acc_grad_indices
-        config.donate_invars
+
+        available_outvars = OrderedSet(compute_outvars)
+        available_outvars.update(global_outvars)
+        for idx in range(len(compute_layers)):
+            if idx not in compute_layer_indices:
+                available_outvars.update(compute_layers[idx].invars)
+        output_vars = OrderedSet()
+        for idx in compute_layer_indices:
+            output_vars.update(compute_layers[idx].outvars)
+        for layer in apply_grad_selected:
+            output_vars.update(layer.outvars)
+        output_vars.intersection_update(available_outvars)
+        output_avals = [var.aval for var in output_vars]
+        self._assert_avals_allmatch(output_avals, config.output_avals)
+        # Check of the two below is based on that donated inputs/outputs are
+        # prior than others.
+        backward_outs = OrderedSet()
+        for idx in indices:
+            backward_idx = len(compute_layers) - 1 - idx
+            backward_outs.update(compute_layers[backward_idx].outvars)
+        backward_outs.intersection_update(compute_outvars)
+        assert config.output_acc_grad_indices == list(range(len(backward_outs)))
+
+        assert len(config.donate_invars) == len(config.input_avals)
+        inputs = OrderedSet()
+        for idx in compute_layer_indices:
+            inputs.update(compute_layers[idx].invars)
+        for layer in apply_grad_selected:
+            inputs.update(layer.invars)
+        inputs.intersection_update(
+            OrderedSet(donation_mapping.keys()).union(
+                apply_grad_donate_map.keys()))
+        donate_invars = len(inputs)
+        for i, donate in enumerate(config.donate_invars):
+            assert (i >= donate_invars) ^ donate
         # check profile config
+        config: ProfileConfig = stage_config.profile_config
+        input_vars = OrderedSet()
+        for idx in compute_layer_indices:
+            input_vars.update(compute_layers[idx].invars)
+        for idx in compute_layer_indices:
+            input_vars.difference_update(compute_layers[idx].outvars)
+        input_avals = [var.aval for var in input_vars]
+        self._assert_avals_allmatch(input_avals, config.input_avals)
+
+        output_vars = OrderedSet()
+        for idx in compute_layer_indices:
+            output_vars.update(compute_layers[idx].outvars)
+        output_vars.intersection_update(available_outvars)
+        output_avals = [var.aval for var in output_vars]
+        self._assert_avals_allmatch(output_avals, config.output_avals)
+        config.donate_invars
 
     def test_generate_stage_config(self):
         (closed_jaxpr, output_tree,
-         donated_invars) = self._create_n_layer_jaxpr_with_donation(n_layers=3)
+         donated_invars) = self._create_n_layer_jaxpr_with_donation(n_layers=4)
         info = self._post_process_jaxpr(closed_jaxpr, donated_invars)
         self._test_generate_stage_config_indices(info, [0])
         self._test_generate_stage_config_indices(info, [0, 1])
-
+        self._test_generate_stage_config_indices(info, [1, 2])
+        self._test_generate_stage_config_indices(info, [2, 3])
 
 def suite():
     suite = unittest.TestSuite()

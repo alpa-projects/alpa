c3328b21c7997dfc4fea8df686874d9f7511f992
Zhuohan Li <zhuohan123@vip.qq.com>
2022-01-12 00:48:53 +0000

    fix

--- tests/test_pipeline_marker.py
+++ tests/test_pipeline_marker.py
@@ -3,26 +3,28 @@ import unittest
 import numpy as np
 
 from parax.pipeline_parallel.primitive_def import mark_pipeline_xla
-from jax.lib import xla_client, xla_bridge
+from jax.lib import xla_client as xc, xla_bridge as xb
 
-ops = xla_client.ops
+ops = xc.ops
 
 
 class PipelineMarkerTest(unittest.TestCase):
 
     def test_xla_graph(self):
-        c = xla_client.XlaBuilder("simple_graph")
-        x = ops.Parameter(c, 0, xla_client.shape_from_pyval(
+        c = xc.XlaBuilder("simple_graph")
+        x = ops.Parameter(c, 0, xc.shape_from_pyval(
             np.ones((10, 8), dtype=np.float32)))
-        y = ops.Parameter(c, 1, xla_client.shape_from_pyval(
+        y = ops.Parameter(c, 1, xc.shape_from_pyval(
             np.ones((10, 8), dtype=np.float32)))
 
-        backend = xla_client.get_local_backend("gpu")
+        backend = xb.get_backend("gpu")
 
         a = ops.Add(x, y)
         b = ops.Mul(x, y)
 
-        a, b = mark_pipeline_xla(c, a, b)
+        output_tuple = mark_pipeline_xla(c, a, b, mark_type="start")
+        a = ops.GetTupleElement(output_tuple, 0)
+        b = ops.GetTupleElement(output_tuple, 1)
 
         z = ops.Add(a, b)
 
@@ -45,8 +47,7 @@ class PipelineMarkerTest(unittest.TestCase):
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(PipelineMarkerTest("test_2_layer_mlp_local_pipeline_parallel"))
-    suite.addTest(PipelineMarkerTest("test_2_layer_mlp_3d_parallel"))
+    suite.addTest(PipelineMarkerTest("test_xla_graph"))
     return suite
 
 

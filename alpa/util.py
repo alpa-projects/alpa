5db95bdd792f571bb6edb958fcb36c0102d572ff
Zhuohan Li <zhuohan123@gmail.com>
2022-02-02 00:15:08 -0800

    fix treemap

--- alpa/util.py
+++ alpa/util.py
@@ -97,7 +97,11 @@ def abstractify_with_aval(x):
 
 
 def tree_to_nparray(tree):
-    return tree_map(lambda x: np.asarray(x), tree)
+    def convert_to_nparray(x):
+        if hasattr(x, "__array__"):
+            return np.asanyarray(x)
+        return x
+    return tree_map(convert_to_nparray, tree)
 
 ########################################
 ##### Data Structure Utilities

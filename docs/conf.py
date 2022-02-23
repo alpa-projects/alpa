8f7ae8fa29af71e2a8bfa078760e0dc2532cc168
Zhuohan Li <zhuohan123@vip.qq.com>
2022-02-23 00:26:31 -0800

    Change page order and don't run inter_and_intra_parallelism.py (#342)

--- docs/conf.py
+++ docs/conf.py
@@ -51,8 +51,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 within_subsection_order = {
     "tutorials": [
         "getting_started.py",
+        "inter_and_intra_parallelism.py",
         "alpa_vs_pmap.py",
-        "inter_and_intra_parallelism.py"
     ],
 }
 
@@ -92,8 +92,7 @@ sphinx_gallery_conf = {
     'gallery_dirs': ['tutorials'],
     'within_subsection_order': WithinSubsectionOrder,
     'backreferences_dir': 'gen_modules/backreferences',
-    "filename_pattern": os.environ.get("ALPA_TUTORIAL_EXEC_PATTERN", ".py"),
-    'ignore_pattern': r'inter_and_intra_parallelism\.py',
+    "filename_pattern": os.environ.get("ALPA_TUTORIAL_EXEC_PATTERN", r"(?<!inter_and_intra_parallelism).py"),
 }
 
 # configuration for intersphinx: refer to the Python standard library.

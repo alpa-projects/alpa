af7488d5efb077a767b7c02e1e652ba16d7d13e5
Zhuohan Li <zhuohan123@gmail.com>
2022-04-12 20:12:02 -0700

    add gpt scripts

---
+++ osdi22_artifact/gen_data_e2e.py
@@ -0,0 +1,87 @@
+"""The entry point of intra-op + inter-op parallelism benchmark."""
+import argparse
+from datetime import datetime
+import time
+
+import numpy as np
+
+from alpa.util import write_tsv, run_cmd, get_num_hosts_and_num_devices, to_str_round, GB
+
+from benchmark.alpa.benchmark_3d_one_case import benchmark_one_case
+from suite_artifact_e2e_gpt import (artifact_search_e2e_gpt_suite, artifact_result_e2e_gpt_suite)
+
+benchmark_suites = {
+    "gpt.search": artifact_search_e2e_gpt_suite,
+    "gpt.result": artifact_result_e2e_gpt_suite,
+}
+
+method_name = {
+    "gpt.search": "parax.auto",
+    "gpt.result": "parax.auto",
+}
+
+
+def benchmark_one_suite(suite_name, num_hosts, num_devices_per_host, exp_name, output_name, instance="p3.16", niter=3,
+                        use_separate_process=True, disable_tqdm=False):
+    # Get the benchmark suite
+    num_gpus = num_hosts * num_devices_per_host
+    try:
+        suite = benchmark_suites[suite_name][num_gpus]
+    except KeyError:
+        suite = None
+    if not suite:
+        print(f"No available benchmark suite for {suite_name} on {num_gpus} GPUs")
+        exit()
+    run_cmd("mkdir -p tmp")
+
+    model_type = suite_name.split(".")[0]
+
+    # Run all cases
+    for benchmark_case in suite:
+        # Run one case
+        print("Working on case: {}".format(str(benchmark_case)))
+        result = benchmark_one_case(model_type, benchmark_case, niter,
+                                    num_hosts, num_devices_per_host,
+                                    use_separate_process=use_separate_process,
+                                    disable_tqdm=disable_tqdm)
+        (parameter_count, mem_allocated, max_mem_allocated, latencies, tflops,
+         tflops_ckpt, compilation_times, compute_cost_file_name, forward_stage_layer_ids,
+         submesh_shapes, logical_mesh_shapes, autosharding_option_dicts) = result
+
+        # Write to file
+        heads = ["exp_name", "instance", "num_hosts", "num_devices_per_host", "model_name", "method", "value", "time_stamp"]
+        values = [exp_name, instance, num_hosts, num_devices_per_host, model_type, method_name[suite_name],
+                  str({"tflops": tflops_ckpt, "parameter_count": parameter_count}), time.time()]
+        write_tsv(heads, values, output_name)
+        time.sleep(0.1)  # for ctrl+c to work
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--search", action="store_true")
+    parser.add_argument("--niter", type=int, default=3,
+        help="The number of benchmark iterations")
+    parser.add_argument("--no-separate-process", action='store_false',
+                        help="Do not launch separate processes for benchmark."
+                             "Errors in a single case will terminate this script.",
+                        dest='use_separate_process')
+    parser.add_argument("--instance", type=str, default="p3.16")
+    parser.add_argument("--exp-name", type=str, default="e2e")
+    parser.add_argument("--disable-tqdm", action="store_true")
+    args = parser.parse_args()
+
+    cluster_sizes = [(4, 8), (2, 8), (1, 8), (1, 4), (1, 2), (1, 1)]
+    output_name = f"result_{args.exp_name}.tsv"
+
+    # GPT e2e results
+    gpt_suite_name = "gpt.search" if args.search else "gpt.result"
+    for num_hosts, num_devices_per_host in cluster_sizes:
+        benchmark_one_suite(gpt_suite_name,
+                            num_hosts,
+                            num_devices_per_host,
+                            args.exp_name,
+                            output_name,
+                            instance=args.instance,
+                            niter=args.niter,
+                            use_separate_process=args.use_separate_process,
+                            disable_tqdm=args.disable_tqdm)
---
+++ osdi22_artifact/suite_artifact_e2e_gpt.py
@@ -0,0 +1,72 @@
+"""Benchmark suite for auto gpt."""
+from benchmark.alpa.suite_paper_manual_gpt import gpt_specs
+
+dummy_arguments = (0, 0, 0, 0) # LD0, LD1, PD0, PD1, not used for auto
+fixed_params = (False, True, True) # FM, Remat, RS
+max_global_batch_size = 1024
+
+default_overwrite_dict = {
+    "auto_stage_construction_imbalance_tolerance": 1.0,
+    "logical_mesh_search_space": "all",
+    "use_hlo_cost_model": True,
+    "profiling_database_filename": "prof_database_4_node_20220111.pkl",
+    "submesh_choices_mode": "small_power_of_two",
+}
+
+
+def get_auto_test_case(model_name, n_microbatches, num_layers,
+                       pipeline_stage_mode="auto_gpipe",
+                       overwrite_global_config_dict=None):
+    if overwrite_global_config_dict is None:
+        overwrite_global_config_dict = default_overwrite_dict
+    return [(max_global_batch_size, *gpt_specs[model_name],
+             *dummy_arguments, num_layer, n_microbatch, *fixed_params,
+             pipeline_stage_mode, overwrite_global_config_dict)
+            for n_microbatch in n_microbatches
+            for num_layer in num_layers]
+
+
+artifact_search_e2e_gpt_suite = {
+2: (get_auto_test_case("760M", [32, 64, 128, 256], [6]) +
+    get_auto_test_case("760M", [32, 64], [12])),
+4: (get_auto_test_case("1.3B", [32, 64, 128], [6]) +
+    get_auto_test_case("1.3B", [32, 64], [12])),
+8:  get_auto_test_case("2.6B", [128], [8]),
+16: get_auto_test_case("6.7B", [64], [8]),
+32: get_auto_test_case("15B", [128], [16]),
+}
+
+
+artifact_result_e2e_gpt_suite = {
+1: get_auto_test_case("125M", [64], [6]),
+2: get_auto_test_case("350M", [128], [6], "manual_gpipe", {
+    "forward_stage_layer_ids": [[0, 1, 2], [3, 4, 5]],
+    "sub_physical_mesh_shapes": [(1, 1)] * 2,
+    "sub_logical_mesh_shapes": [(1, 1)] * 2,
+    "submesh_autosharding_option_dicts": [{'force_batch_dim_to_mesh_dim': 0}] * 2,
+}),
+4: get_auto_test_case("760M", [128], [6], "manual_gpipe", {
+    "forward_stage_layer_ids": [[0, 1, 2], [3, 4, 5]],
+    "sub_physical_mesh_shapes": [(1, 2)] * 2,
+    "sub_logical_mesh_shapes": [(2, 1)] * 2,
+    "submesh_autosharding_option_dicts": [{'force_batch_dim_to_mesh_dim': 0}] * 2,
+}),
+8: get_auto_test_case("2.6B", [128], [8], "manual_gpipe", {
+    "forward_stage_layer_ids": [[0, 1], [2, 3], [4, 5, 6, 7]],
+    "sub_physical_mesh_shapes": [(1, 2), (1, 2), (1, 4)],
+    "sub_logical_mesh_shapes": [(2, 1), (2, 1), (4, 1)],
+    "submesh_autosharding_option_dicts": [{'force_batch_dim_to_mesh_dim': 0}, {}, {}],
+}),
+16: get_auto_test_case("6.7B", [64], [8], "manual_gpipe", {
+    "forward_stage_layer_ids": [[0, 1, 2, 3], [4, 5, 6, 7]],
+    "sub_physical_mesh_shapes": [(1, 8)] * 2,
+    "sub_logical_mesh_shapes": [(2, 4)] * 2,
+    "submesh_autosharding_option_dicts": [{'force_batch_dim_to_mesh_dim': 0} for _ in range(2)],
+}),
+32: get_auto_test_case("15B", [128], [16], "manual_gpipe", {
+    "forward_stage_layer_ids": [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]],
+    "sub_physical_mesh_shapes": [(1, 8)] * 4,
+    "sub_logical_mesh_shapes": [(2, 4)] * 4,
+    "submesh_autosharding_option_dicts": [{'force_batch_dim_to_mesh_dim': 0} for _ in range(4)],
+}),
+}

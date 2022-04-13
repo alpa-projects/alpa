52d837b1ffc708ccde0953787757e63fa6dc5363
Zhuohan Li <zhuohan123@gmail.com>
2022-04-12 22:05:58 -0700

    fix

--- osdi22_artifact/suite_artifact_e2e_gpt.py
+++ osdi22_artifact/suite_artifact_e2e_gpt.py
@@ -9,7 +9,7 @@ default_overwrite_dict = {
     "auto_stage_construction_imbalance_tolerance": 1.0,
     "logical_mesh_search_space": "all",
     "use_hlo_cost_model": True,
-    "profiling_database_filename": "prof_database_4_node_20220111.pkl",
+    "profiling_database_filename": "prof_database_osdi22_artifact.pkl",
     "submesh_choices_mode": "small_power_of_two",
 }
 
@@ -38,7 +38,12 @@ artifact_search_e2e_gpt_suite = {
 
 
 artifact_result_e2e_gpt_suite = {
-1: get_auto_test_case("125M", [64], [6]),
+1: get_auto_test_case("125M", [64], [1], "manual_gpipe", {
+    "forward_stage_layer_ids": [[0]],
+    "sub_physical_mesh_shapes": [(1, 1)],
+    "sub_logical_mesh_shapes": [(1, 1)],
+    "submesh_autosharding_option_dicts": [{}],
+}),
 2: get_auto_test_case("350M", [128], [6], "manual_gpipe", {
     "forward_stage_layer_ids": [[0, 1, 2], [3, 4, 5]],
     "sub_physical_mesh_shapes": [(1, 1)] * 2,
@@ -49,7 +54,7 @@ artifact_result_e2e_gpt_suite = {
     "forward_stage_layer_ids": [[0, 1, 2], [3, 4, 5]],
     "sub_physical_mesh_shapes": [(1, 2)] * 2,
     "sub_logical_mesh_shapes": [(2, 1)] * 2,
-    "submesh_autosharding_option_dicts": [{'force_batch_dim_to_mesh_dim': 0}] * 2,
+    "submesh_autosharding_option_dicts": [{'force_batch_dim_to_mesh_dim': 0} for _ in range(2)],
 }),
 8: get_auto_test_case("2.6B", [128], [8], "manual_gpipe", {
     "forward_stage_layer_ids": [[0, 1], [2, 3], [4, 5, 6, 7]],

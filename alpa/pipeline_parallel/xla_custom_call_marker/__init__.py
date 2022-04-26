cfc2a4eb69a7687cb00e4bb030fac5921ebb1a96
ZYHowell <zhuangyh@sjtu.edu.cn>
2022-04-26 14:06:48 +0000

    minor fix on pipeline marker

--- alpa/pipeline_parallel/xla_custom_call_marker/__init__.py
+++ alpa/pipeline_parallel/xla_custom_call_marker/__init__.py
@@ -2,7 +2,7 @@ try:
     from .build.xla_custom_call_marker import pipeline_marker, identity
 except ImportError as e:
     import os
-    print("Cannot import XLA custom markers")
+    print(f"Cannot import XLA custom markers: {e}")
     path = os.path.dirname(__file__)
     print(f"Please run 'bash build.sh' under {path}")
     exit(-1)

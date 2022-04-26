d67f74293ae3e226cc9d3401b835e2c956038a80
Hao Zhang <zhisbug@gmail.com>
2022-04-25 21:06:42 -0700

    minor fixs

--- alpa/pipeline_parallel/xla_custom_call_marker/build.sh
+++ alpa/pipeline_parallel/xla_custom_call_marker/build.sh
@@ -1,6 +1,6 @@
 mkdir -p build
 cd build
-cmake .. -Dpybind11_ROOT=$(pybind11-config --cmakedir)
+cmake .. -DPYTHON_EXECUTABLE=$(which python) -Dpybind11_ROOT=$(pybind11-config --cmakedir)
 export PATH=/usr/local/cuda/bin:$PATH
 export CPATH=/usr/local/cuda/targets/x86_64-linux/include/:$CPATH
 make

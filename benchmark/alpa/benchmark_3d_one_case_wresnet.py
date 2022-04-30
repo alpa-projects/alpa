f88017ba43b86575003c7a3477343870100d1872
Yonghao Zhuang <zhuangyh@sjtu.edu.cn>
2022-04-30 12:37:02 +0800

    [BENCHMARK] update wresnet benchmark result suite (#412)

--- benchmark/alpa/benchmark_3d_one_case_wresnet.py
+++ benchmark/alpa/benchmark_3d_one_case_wresnet.py
@@ -1,25 +1,22 @@
 """Benchmark one case of inter-op + intra-op parallelism."""
 from functools import partial
 
-from flax import linen as nn, optim
 from flax.training import common_utils
 import jax
 import jax.numpy as jnp
 import numpy as np
 import optax
-import ray
 
 import alpa
-from alpa import (parallelize, global_config, set_parallelize_options, testing,
-                   DeviceCluster, automatic_layer_construction)
+from alpa import (parallelize, global_config, set_parallelize_options,
+                  DeviceCluster, automatic_layer_construction)
 from alpa.model.wide_resnet import get_wide_resnet, TrainState
+from alpa.pipeline_parallel.layer_construction import automatic_remat
 from alpa.pipeline_parallel.stage_construction import get_last_dp_result
 from alpa.timer import timers
-from alpa.util import map_to_shape, print_used_time, compute_param_number, GB, to_str_round
-
+from alpa.util import print_used_time, compute_param_number, to_str_round
 
 resnet_layer_to_alpa_layer = {50: 16, 101: 33}
-
 
 as_option = global_config.default_autosharding_option
 
@@ -82,12 +79,15 @@ def create_train_state(rngkey, model, input_images, learning_rate_fn):
 
 def get_train_step(learning_rate_fn, use_grad_acc, use_remat, num_layers):
 
+    layer_num = resnet_layer_to_alpa_layer[num_layers]
+    if global_config.forward_stage_layer_ids:
+        fwd_last_layer = global_config.forward_stage_layer_ids[-1][-1] + 1
+        if fwd_last_layer > layer_num:
+            layer_num = fwd_last_layer
+
     @parallelize
     def train_step(state, batch):
 
-        @partial(automatic_layer_construction,
-                 layer_num=resnet_layer_to_alpa_layer[num_layers],
-                 remat_layer=use_remat)
         def loss_fn(params):
             logits, new_model_state = state.apply_fn(
                 {
@@ -108,6 +108,14 @@ def get_train_step(learning_rate_fn, use_grad_acc, use_remat, num_layers):
                 "lr": learning_rate_fn(step)
             }
             return loss, (new_model_state, metrics)
+
+        if global_config.strategy == "shard_parallel":
+            if use_remat:
+                loss_fn = automatic_remat(loss_fn, layer_num=layer_num)
+        else:
+            loss_fn = automatic_layer_construction(loss_fn,
+                                                   remat_layer=use_remat,
+                                                   layer_num=layer_num)
 
         step = state.step
         dynamic_scale = state.dynamic_scale
@@ -145,15 +153,16 @@ def get_train_step(learning_rate_fn, use_grad_acc, use_remat, num_layers):
     return train_step
 
 
-def benchmark_wresnet_internal(benchmark_case, niter,
-                               num_hosts, num_devices_per_host):
+def benchmark_wresnet_internal(benchmark_case, niter, num_hosts,
+                               num_devices_per_host):
     print_used_time(None)
 
     # Model configs
     model_type = "wide_resnet"
     batch_size, image_size, num_layers, num_channels, width_factor, dtype,\
         num_micro_batches, force_batch_dim_mapping,\
-        prefer_reduce_scatter, use_remat, logical_mesh_search_space = benchmark_case
+        prefer_reduce_scatter, use_remat, logical_mesh_search_space,\
+        overwrite_global_config_dict = benchmark_case
     pipeline_stage_mode = "auto_gpipe"
     if dtype == "fp32":
         dtype = jnp.float32
@@ -183,6 +192,11 @@ def benchmark_wresnet_internal(benchmark_case, niter,
                             logical_mesh_search_space=logical_mesh_search_space)
     global_config.auto_stage_construction_imbalance_tolerance = 0.25
 
+    if isinstance(overwrite_global_config_dict, dict):
+        global_config.update_with_dict(overwrite_global_config_dict)
+    if global_config.strategy == "shard_parallel":
+        global_config.devices = virtual_mesh.get_physical_mesh()
+
     # Prepare input batch
     num_classes = 1024
     batch = {
@@ -202,7 +216,8 @@ def benchmark_wresnet_internal(benchmark_case, niter,
     learning_rate_fn = create_learning_rate_fn()
     rngkey = jax.random.PRNGKey(0)
     state = create_train_state(rngkey, model, batch["images"], learning_rate_fn)
-    train_step = get_train_step(learning_rate_fn, use_grad_acc, use_remat, num_layers)
+    train_step = get_train_step(learning_rate_fn, use_grad_acc, use_remat,
+                                num_layers)
     print_used_time("Create train state")
     parameter_count = compute_param_number(state.params)
 
@@ -210,20 +225,28 @@ def benchmark_wresnet_internal(benchmark_case, niter,
     executable = train_step.get_executable(state, batch)
     print_used_time("Compile (driver)")
 
-    compilation_times = {k : timers(k).elapsed() for k in
-            ["stage-construction", "stage-construction-dp",
-             "stage-construction-compilation", "stage-construction-profiling"]}
-    print(f"compilation time breakdown: {to_str_round(compilation_times, 2)}")
+    if global_config.strategy == "pipeshard_parallel":
+        compilation_times = {
+            k: timers(k).elapsed() for k in [
+                "stage-construction", "stage-construction-dp",
+                "stage-construction-compilation", "stage-construction-profiling"
+            ]
+        }
+        print(
+            f"compilation time breakdown: {to_str_round(compilation_times, 2)}")
 
-    # Dump hlo ir for debugging
-    stage_hlo_texts = executable.get_hlo_text()
-    for i in range(len(stage_hlo_texts)):
-        with open(f"tmp/stage_{i}.hlo", "w") as fout:
-            fout.write(stage_hlo_texts[i])
-    with open(f"tmp/resharding_tasks.txt", "w") as fout:
-        fout.write(executable.print_resharding_tasks())
+        # Dump hlo ir for debugging
+        stage_hlo_texts = executable.get_hlo_text()
+        for i in range(len(stage_hlo_texts)):
+            with open(f"tmp/stage_{i}.hlo", "w") as fout:
+                fout.write(stage_hlo_texts[i])
+        with open(f"tmp/resharding_tasks.txt", "w") as fout:
+            fout.write(executable.print_resharding_tasks())
 
-    executable.sync()
+        executable.sync()
+    elif global_config.strategy == "shard_parallel":
+        global_config.devices.sync_workers()
+        compilation_times = {}
     print_used_time("Compile (workers)")
 
     # Benchmark step time
@@ -240,8 +263,12 @@ def benchmark_wresnet_internal(benchmark_case, niter,
     latencies = executable.get_execution_time_costs(warmup=2)
     print_used_time("Benchmark")
 
-    mem_allocated = executable.get_memory_allocated()
-    max_mem_allocated = executable.get_max_memory_allocated()
+    if global_config.strategy == "pipeshard_parallel":
+        mem_allocated = executable.get_memory_allocated()
+        max_mem_allocated = executable.get_max_memory_allocated()
+    elif global_config.strategy == "shard_parallel":
+        mem_allocated = executable.get_total_allocation_size()
+        max_mem_allocated = global_config.devices.get_max_memory_allocated()
 
     # Compute statistics
     num_gpus = virtual_mesh.num_devices
@@ -253,7 +280,10 @@ def benchmark_wresnet_internal(benchmark_case, niter,
     #     for k in profiled:
     #         pstr += f"Exec {k}: {profiled[k][0]}s; "
     #     print(pstr)
-    executable.shutdown()
+    if global_config.strategy == "pipeshard_parallel":
+        executable.shutdown()
+    elif global_config.strategy == "shard_parallel":
+        global_config.devices.shutdown()
 
     return (parameter_count, mem_allocated, max_mem_allocated, latencies,
             tflops, tflops, compilation_times) + get_last_dp_result()

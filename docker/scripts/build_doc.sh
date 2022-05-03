e75c3ffbdcdc3c74ec2f83c2d133a43b491bf8d2
Hao Zhang <zhisbug@gmail.com>
2022-05-03 13:58:38 -0700

    do not mount to docker

--- docker/scripts/build_doc.sh
+++ docker/scripts/build_doc.sh
@@ -16,8 +16,10 @@ fi
 
 pip install /alpa-dist/jaxlib-alpa/jaxlib-0.3.5-cp38-none-manylinux2010_x86_64.whl
 pip install /alpa-dist/jax-alpa/jax-0.3.5.tar.gz
-pip install -e .
 
-ls /alpa
+git clone https://github.com/alpa-projects/alpa.git
+cd alpa
+pip install -e .
 cd /alpa/docs
 make html
+

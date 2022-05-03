d511a545b46cd1c40fa3d5d35eb660191b59d169
Hao Zhang <zhisbug@gmail.com>
2022-05-03 13:59:59 -0700

    update

--- docker/scripts/build_doc.sh
+++ docker/scripts/build_doc.sh
@@ -8,12 +8,6 @@ then
   exit 1
 fi
 
-if [ ! -d "/alpa" ]
-then
-  echo "/alpa must be mounted to produce output"
-  exit 1
-fi
-
 pip install /alpa-dist/jaxlib-alpa/jaxlib-0.3.5-cp38-none-manylinux2010_x86_64.whl
 pip install /alpa-dist/jax-alpa/jax-0.3.5.tar.gz
 

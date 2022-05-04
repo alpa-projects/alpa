fd11a191d8eebc35ba14248d9db92022cf1d8b18
Hao Zhang <zhisbug@gmail.com>
2022-05-03 19:12:31 -0700

    update

--- docker/scripts/build_doc.sh
+++ docker/scripts/build_doc.sh
@@ -12,8 +12,9 @@ source /python3.8-env/bin/activate
 pip install /alpa-dist/jaxlib-alpa/jaxlib-0.3.5-cp38-none-manylinux2010_x86_64.whl
 pip install /alpa-dist/jax-alpa/jax-0.3.5.tar.gz
 
-git clone -b hao-docs https://github.com/alpa-projects/alpa.git
+git clone https://github.com/alpa-projects/alpa.git
 cd alpa
 pip install -e .[doc]
 cd /alpa/docs
 make html
+cp -r _build/html/* /alpa-dist/docs/

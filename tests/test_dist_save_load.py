7d7efee409921f1a4103543c475f650605cd3e8d
Zhong Yinmin <zhongyinmin@pku.edu.cn>
2022-05-08 12:44:42 +0000

    support bert distributed save/load

--- tests/test_dist_save_load.py
+++ tests/test_dist_save_load.py
@@ -10,12 +10,15 @@ import jax
 import jax.numpy as jnp
 from jax.tree_util import tree_flatten, tree_unflatten, PyTreeDef
 import numpy as np
+import optax
 import ray
 
-from alpa import DeviceCluster, DistributedArray, parallelize, set_parallelize_options, save_checkpoint, restore_checkpoint
+from alpa import DeviceCluster, DistributedArray, parallelize, set_parallelize_options, save_checkpoint, restore_checkpoint, manual_layer_construction
 from alpa.global_env import set_parallelize_options, global_config
+from alpa.model.bert_model import BertConfig
+from alpa.model.model_util import TrainState
 from alpa.pipeline_parallel.primitive_def import mark_pipeline
-from alpa.testing import (MLPModel, create_train_state, get_mlp_train_step,
+from alpa.testing import (MLPModel, BertLayerModel, create_train_state, get_bert_layer_train_step, get_mlp_train_step,
                           assert_allclose)
 
 
@@ -64,8 +67,8 @@ class DistSaveLoadTest(unittest.TestCase):
             (input_indices,), (input_sharding_spec,), (global_input_data1,))
 
         # Check the DistributedArray's remote buffers
-        desired_buffers1 = [[[0], [2]], [[1], [3]], [[4], [6]], [[5], [7]]]
-        self.check_dist_array_eq(np.array(desired_buffers1), dist_input_data1)
+        desired_buffers1 = np.array([[[0], [2]], [[1], [3]], [[4], [6]], [[5], [7]]])
+        self.check_dist_array_eq(desired_buffers1, dist_input_data1)
 
         # Save the DistributedArray (one replica only)
         tmpdir = tempfile.TemporaryDirectory()
@@ -84,10 +87,10 @@ class DistSaveLoadTest(unittest.TestCase):
             physical_mesh, load_sharding_spec)
 
         # Check the DistributedArray's remote buffers
-        desired_buffers2 = [[[0, 1], [2, 3]], [[0, 1], [2, 3]], [[4, 5], [6,
+        desired_buffers2 = np.array([[[0, 1], [2, 3]], [[0, 1], [2, 3]], [[4, 5], [6,
                                                                           7]],
-                            [[4, 5], [6, 7]]]
-        self.check_dist_array_eq(np.array(desired_buffers2), dist_load_data1)
+                            [[4, 5], [6, 7]]])
+        self.check_dist_array_eq(desired_buffers2, dist_load_data1)
 
         # Cleanup
         physical_mesh.shutdown()
@@ -105,6 +108,8 @@ class DistSaveLoadTest(unittest.TestCase):
         model = MLPModel(hidden_dim=hidden_dim,
                          output_dim=output_dim,
                          manual_pipeline_layer=True)
+        
+        # Init batch args
         rngkey = jax.random.PRNGKey(0)
         x = jax.random.normal(rngkey, (batch_size, input_dim))
         y = jax.random.normal(rngkey, (batch_size, output_dim))
@@ -135,15 +140,89 @@ class DistSaveLoadTest(unittest.TestCase):
         # Run after load
         serial_state = serial_train_step(serial_state, batch)[0]
         load_state = parallel_train_step(load_state, batch)[0]
+        
+        # Check results
         assert_allclose(serial_state.params, load_state.params, 1e-3, 1e-3)
 
         # Cleanup
         executable.shutdown()
+    
+    def test_distributed_bert_save_load(self):
+        # Set pipeline parallel option
+        virtual_mesh = DeviceCluster().get_virtual_physical_mesh()
+        set_parallelize_options(devices=virtual_mesh,
+                                strategy="pipeshard_parallel")
+
+        # Config
+        batch_size = 16
+        seq_len = 8
+        hidden_size = 128
+        num_heads = 8
+        n_layers = 4
+        dtype = jnp.float32
+
+        # Init batch args
+        rngkey = jax.random.PRNGKey(0)
+        x = jax.random.normal(rngkey, (batch_size, seq_len, hidden_size),
+                              dtype=dtype)
+        y = jax.random.normal(rngkey, (batch_size, seq_len, hidden_size),
+                              dtype=dtype)
+        attention_mask = jnp.ones((batch_size, seq_len), dtype=dtype)
+        batch = {"x": x, "y": y, "attention_mask": attention_mask}
+
+        # Init model and optimizer
+        model = BertLayerModel(config=BertConfig(hidden_size=hidden_size,
+                                                 intermediate_size=hidden_size *
+                                                 4,
+                                                 num_attention_heads=num_heads,
+                                                 num_hidden_layers=n_layers))
+        rngkey = jax.random.PRNGKey(0)
+        params = model.init(rngkey, x, attention_mask)
+        tx = optax.sgd(learning_rate=1e-2)
+        state = TrainState.create(apply_fn=model.apply,
+                                  params=params,
+                                  tx=tx,
+                                  dynamic_scale=None)
+
+        # Compile
+        global_config.num_micro_batches = 2
+        serial_train_step = get_bert_layer_train_step(False, None, None, n_layers, False)
+        parallel_train_step = get_bert_layer_train_step(True, True, False, n_layers, False)
+        executable = parallel_train_step.get_executable(state, batch)
+
+        # Run before save
+        serial_state = state
+        parallel_state = state
+        serial_state = serial_train_step(serial_state, batch)[0]
+        parallel_state = parallel_train_step(parallel_state, batch)[0]
+        assert_allclose(serial_state.params, parallel_state.params, 1e-3, 1e-3)
+
+        with tempfile.TemporaryDirectory(prefix="/home/ubuntu/efs/") as ckpt_dir:
+            # Save checkpoint 
+            save_checkpoint(ckpt_dir, parallel_state, 1)
+
+            # Restore checkpoint
+            load_path_dict = restore_checkpoint(ckpt_dir, state, 1)
+            load_state = executable.load_state_dict(load_path_dict)
+
+        # Run after load
+        serial_state = serial_train_step(serial_state, batch)[0]
+        load_state = parallel_train_step(load_state, batch)[0]
+
+        # Check results
+        assert_allclose(serial_state.params, load_state.params, 1e-3, 1e-3)
+        
+        # Cleanup
+        executable.shutdown()
+
+
+
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(DistSaveLoadTest("test_distributed_array_save_load"))
     suite.addTest(DistSaveLoadTest("test_distributed_mlp_save_load"))
+    suite.addTest(DistSaveLoadTest("test_distributed_bert_save_load"))
     return suite
 
 

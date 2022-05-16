89db2f1fcdd02fc68d456512aa97603557f69833
Hao Zhang <zhisbug@gmail.com>
2022-05-15 21:36:34 -0700

    more experiments

--- examples/opt/inspect_weights.py
+++ examples/opt/inspect_weights.py
@@ -1,2 +1,19 @@
-raw_weights_path = "/home/ubuntu/parax-efs/pycharm/opt/raw_weights"
+import os
+
+from metaseq.checkpoint_utils import load_checkpoint_to_cpu, torch_load_cpu
+
+# raw_weights_path = "/home/ubuntu/parax-efs/pycharm/opt/raw_weights/"
+raw_weights_path = "/home/ubuntu/opt/reshard.pt"
+# raw_weights_path = "/home/ubuntu/parax-efs/pycharm/opt/raw_weights/1.3B/checkpoint_last-model_part-0-shard0.pt"
+vocab_file = os.path.join("/home/ubuntu/parax-efs/pycharm/opt/raw_weights/1.3B/gpt2-vocab.json")
+merges_file = os.path.join("/home/ubuntu/parax-efs/pycharm/opt/raw_weights/1.3B/gpt2-merges.txt")
+arg_overrides={"vocab_filename": vocab_file, "merge_filename": merges_file}
+
+state = load_checkpoint_to_cpu(raw_weights_path, arg_overrides=arg_overrides)
+print(state)
+print(state["model"])
+
+
+
+
 

fix speed calculation

playground/model/test_speed.py
@@ -17,7 +17,7 @@ GB = 1 << 30
 
 
 def test_load(name, dummy):
-    config = get_config(name, num_pp_stages=2)
+    config = get_config(name, num_pp_stages=4)
     path = f"/home/ubuntu/opt_weights/{name}_np"
 
     alpa.init()
@@ -42,27 +42,34 @@ def test_load(name, dummy):
 
     print("Build cache...")
     tic = time.time()
-    cache = init_cache_dis_array(executable, config, dummy=dummy)
+    init_cache = init_cache_dis_array(executable, config, dummy=dummy)
     executable.sync()
     duration = time.time() - tic
-    num_bytes = compute_bytes(cache)
+    num_bytes = compute_bytes(init_cache)
     print(f"Duration: {duration:.2f}, Bandwidth: {num_bytes / duration / GB:.2f} GB/s")
 
     # Run
-    for i in range(input_ids.shape[1]):
-        tic = time.time()
-        input_ids_step = input_ids[:, i:i+1]
-        position_ids_step = np.full_like(input_ids_step, i + config.pad + 1)
-        logits_step, cache = executable(params, {
-            "input_ids": input_ids_step,
-            "position_ids": position_ids_step,
-            "cache": cache,
-        })
+    for _ in range(2):
+        start_time = time.time()
+        cache = init_cache
+        for i in range(input_ids.shape[1]):
+            tic = time.time()
+            input_ids_step = input_ids[:, i:i+1]
+            position_ids_step = np.full_like(input_ids_step, i + config.pad + 1)
+            output = executable(params, {
+                "input_ids": input_ids_step,
+                "position_ids": position_ids_step,
+                "cache": cache,
+            })
+            cache = output.attention_cache
 
-        logits_step = str(logits_step).replace("\n", " ")
-        print(f"step: {i}, latency: {time.time() - tic:.2f}, "
-              f"exec_cost: {executable.get_execution_time_costs(warmup=0)[-1]:.2f}, "
-              f"logits: {logits_step}")
+            logits_step = str(output.logits).replace("\n", " ")
+            print(f"step: {i}, latency: {time.time() - tic:.2f}, "
+                  f"exec_cost: {executable.get_execution_time_costs(warmup=0)[-1]:.2f}, "
+                  f"logits: {logits_step}")
+        duration = time.time() - start_time
+        latency = duration / np.prod(input_ids.shape)
+        print(f"latency: {latency:.2f} s/token, throughput: {1/latency:.2f} token/s")
 
 
 if __name__ == "__main__":

playground/model/test_text_gen.py
@@ -133,11 +133,13 @@ def get_model(model_name, support_output_attentions=False,
         print(inference_func_config)
 
     elif "alpa/opt" in model_name:
+        alpa.init()
+        num_pp_stages = max(2, alpa.get_global_cluster().num_hosts)
+
         name = model_name.split("-")[1].upper()
-        config = get_config(name, num_pp_stages=2)
+        config = get_config(name, num_pp_stages=num_pp_stages)
         path = f"/home/ubuntu/opt_weights/{name}_np"
 
-        alpa.init()
         dummy = False
         executable, params_aval = get_pipeshard_executable(
             config,
@@ -164,12 +166,11 @@ def get_model(model_name, support_output_attentions=False,
                 "position_ids": position_ids_step,
                 "cache": past_key_values,
             })
-            logits_step = output.logits
-            logits_step = torch.from_numpy(np.array(logits_step))
-            past_key_values = output.attention_cache
+            logits_step = torch.from_numpy(np.array(output.logits))
 
             step_ct += 1
-            return InferenceFuncOutput(logits_step, past_key_values,
+            return InferenceFuncOutput(logits_step,
+                                       output.attention_cache,
                                        output.hidden_states,
                                        output.attentions)
 
@@ -179,7 +180,7 @@ def get_model(model_name, support_output_attentions=False,
 
 
 tokenizer = GPT2Tokenizer.from_pretrained("facebook/opt-125m")
-model = get_model("alpa/opt-125M", support_output_hidden_states=True)
+model = get_model("alpa/opt-125M", support_output_hidden_states=False)
 
 prompts = [
     "Computer science is the study of computation and",
@@ -196,4 +197,4 @@ for prompt in prompts:
     generated_ids = output.sequences
     generated_string = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
     duration = time.time() - tic
-    print(f"{generated_string}, speed: {len(generated_ids)/duration:.2f} token/s")
+    print(f"{generated_string}, speed: {np.prod(generated_ids.shape)/duration:.2f} token/s")
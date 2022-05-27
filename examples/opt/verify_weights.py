402101c51c629d938c112f4a30e168e8eecbb37e
Hao Zhang <sjtu.haozhang@gmail.com>
2022-05-27 00:33:14 -0700

    verify weight script

--- examples/opt/verify_weights.py
+++ examples/opt/verify_weights.py
@@ -1,26 +1,24 @@
-import os
-
-import torch
 import numpy as np
-from metaseq.checkpoint_utils import load_checkpoint_to_cpu, torch_load_cpu
-
+import os
+from metaseq.checkpoint_utils import torch_load_cpu
 
 # Replace this with the path to the eight parts
 path = "/home/ubuntu/dataset/175B-resharded/renamed/"
-weigth_paths = [os.path.join(path, f"reshard-model_part-{part_idx}.pt") for part_idx in range(8)]
+weight_paths = [os.path.join(path, f"reshard-model_part-{part_idx}.pt") for part_idx in range(8)]
 
 result_file = "/tmp/results"
 np.random.seed(9)
 
-
 with open(result_file, "w") as rf:
-    for i, weight_path in enumerate(weigth_paths):
-        print(f"Processing MP part {i}...")
+    rf.write(f"All MP parts: {weight_paths}.\n")
+    for i, weight_path in enumerate(weight_paths):
+        rf.write(f"> Processing MP part {i}...\n")
         part = torch_load_cpu(weight_path)
         assert "model" in part.keys()
         flat_params = part["model"]
         for param in flat_params:
-            rf.write(f"Verifying {param} with size: {param.size}:")
-            positions = np.random.randint(low=0, high=param.size, size=500)
-            rf.write(f">>> positions:: {positions}")
-            rf.write(f">>> weights: {param[positions]}")
+            param_size = flat_params[param].size()
+            rf.write(f">> Verifying {param} with size: {param_size}:\n")
+            positions = np.random.randint(low=0, high=param_size, size=50)
+            rf.write(f">>> positions:: {positions}\n")
+            rf.write(f">>> weights: {flat_params[param][positions]}\n")

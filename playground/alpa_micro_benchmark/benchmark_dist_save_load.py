0370c17020a43670b181d2725edb18af15d72978
Zhong Yinmin <zhongyinmin@pku.edu.cn>
2022-06-04 12:09:09 +0000

    benchmark original distarr save/load

--- playground/alpa_micro_benchmark/benchmark_dist_save_load.py
+++ playground/alpa_micro_benchmark/benchmark_dist_save_load.py
@@ -14,7 +14,7 @@ import numpy as np
 import alpa
 from alpa import save_checkpoint as alpa_save_checkpoint
 from alpa import restore_checkpoint as alpa_restore_checkpoint
-from alpa import PipeshardParallel  
+from alpa import PipeshardParallel, DistributedArray
 from alpa.testing import (MLPModel, BertLayerModel, create_train_state,
                           get_bert_layer_train_step, get_mlp_train_step,
                           assert_allclose)
@@ -37,7 +37,7 @@ def _get_save_prefix(to_efs):
         save_prefix = "/tmp/"
     return save_prefix
 
-LOOP_CNT=5
+LOOP_CNT=1
 
 def benchmark_ndarray_save_load(mode="flax", to_efs=True):
     """
@@ -52,6 +52,7 @@ def benchmark_ndarray_save_load(mode="flax", to_efs=True):
     - flax.restore_checkpoint: load average run time:  6.8287 seconds, load average throughput: 1.2225 Gbps
 
     - alpa.save_checkpoint:    save average run time: 12.8583 seconds, save average throughput: 0.6222 Gbps
+                 use cache:    
     - alpa.restore_checkpoint: N/A
 
     - np.save:                 save average run time: 10.4157 seconds, save average throughput: 0.7682 Gbps
@@ -92,7 +93,7 @@ def benchmark_ndarray_save_load(mode="flax", to_efs=True):
             if mode == "flax":
                 save_checkpoint(outdir, arr, i)
             elif mode == "alpa":
-                alpa_save_checkpoint(outdir, arr, i)
+                alpa_save_checkpoint(outdir, arr, i, "/tmp")
             else:
                 np.save(ckpt_path, arr)
             duration = time.time() - start
@@ -127,6 +128,7 @@ def benchmark_mlp_save(mode="flax", to_efs=True):
     Benchmark results on EFS: 
     - flax.save_checkpoint: average run time: 45.19087886810303 seconds, average throughput: 0.5313484040513637 Gbps
     - alpa.save_checkpoint: average run time: 16.15189399719238, average throughput: 1.4860819837013484 Gbps
+                 use cache: 
     - np.save:              average run time: 20.618193340301513, average throughput: 1.1642373201358331 Gbps
 
     Benchmark results on local disk:
@@ -166,7 +168,7 @@ def benchmark_mlp_save(mode="flax", to_efs=True):
         if mode == "flax":
             save_checkpoint(outdir, state, i)
         elif mode == "alpa":
-            alpa_save_checkpoint(outdir, state, i)
+            alpa_save_checkpoint(outdir, state, i, "/tmp")
         else:
             np.save(ckpt_path, state.params)
             np.save(ckpt_path, state.opt_state)
@@ -177,6 +179,62 @@ def benchmark_mlp_save(mode="flax", to_efs=True):
         tot_throughput += throughput
         print(f"loop {i}, time: {duration} seconds, throughput: {throughput} Gbps")
     print(f"average run time: {tot_duration/LOOP_CNT}, average throughput: {tot_throughput/LOOP_CNT} Gbps")
+
+def benchmark_dist_arr_save_load(to_efs=False):
+    """
+    Benchmark results on local disk:
+    - one host:
+        save average run time: 28.0506 seconds, save average throughput: 0.2852 Gbps
+        load average run time: 2.3506 seconds, load average throughput: 3.4035 Gbps
+
+    - two hosts:
+        save average run time: 16.3754 seconds, save average throughput: 0.4885 Gbps
+        load average run time: 1.2287 seconds, load average throughput: 6.5110 Gbps
+    """
+    device_cluster = get_global_cluster()
+    physical_mesh = device_cluster.get_physical_mesh()
+    logical_mesh = physical_mesh.get_logical_mesh()
+
+    rngkey = random.PRNGKey(0)
+    arr_shape = (16*1024, 16*1024) #1GB
+    arr = random.normal(rngkey, arr_shape)
+
+    sharding_spec = logical_mesh.make_tile_spec(arr, [0, 1], [0, 1])
+    input_indices = sharding_spec.indices(arr.shape).flatten()
+    (dist_arr,) = physical_mesh.shard_args_to_arrays(
+        (jax.ShapedArray(arr.shape, jnp.int32),),
+        (input_indices,), (sharding_spec,), (arr,))
+
+    save_prefix = _get_save_prefix(to_efs)
+    save_tot_duration = 0.0
+    save_tot_throughput = 0.0
+    load_tot_duration = 0.0
+    load_tot_throughput = 0.0
+    for i in range(LOOP_CNT):
+        # Save the DistributedArray (one replica only)
+        outdir = TemporaryDirectory(prefix=save_prefix)
+        subprocess.run(["rm", "-rf", outdir.name])
+        print(f"save to {outdir}")
+
+        # save benchmark
+        start = time.time()
+        dist_arr.save(outdir.name)
+        duration = time.time() - start
+        throughput = arr.size * 32 / 1024 / 1024 / 1024 / duration
+        save_tot_duration += duration
+        save_tot_throughput += throughput
+        print(f"loop {i} save, time: {duration:.4f} seconds, throughput: {throughput:.4f} Gbps")
+
+        # load benchmark
+        start = time.time()
+        _ = DistributedArray.load(outdir.name, jax.ShapedArray(arr.shape, jnp.int32), physical_mesh, sharding_spec)
+        duration = time.time() - start
+        throughput = arr.size * 32 / 1024 / 1024 / 1024 / duration
+        load_tot_duration += duration
+        load_tot_throughput += throughput
+        print(f"loop {i} load, time: {duration:.4f} seconds, throughput: {throughput:.4f} Gbps")
+    print(f"save average run time: {save_tot_duration/LOOP_CNT:.4f} seconds, save average throughput: {save_tot_throughput/LOOP_CNT:.4f} Gbps")
+    print(f"load average run time: {load_tot_duration/LOOP_CNT:.4f} seconds, load average throughput: {load_tot_throughput/LOOP_CNT:.4f} Gbps")
 
 def benchmark_mlp_dist_save_load():
     """
@@ -271,8 +329,9 @@ if __name__ == "__main__":
     # benchmark_mlp_save(mode="numpy", to_efs=False)
 
     # print("mlp dist save/load benchmark:")
-    benchmark_mlp_dist_save_load()
+    # benchmark_mlp_dist_save_load()
 
+    benchmark_dist_arr_save_load()
     alpa.shutdown()
     
 

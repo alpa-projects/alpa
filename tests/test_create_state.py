efd4d3c81625c60de485c66ace9551cfab85b28f
Lianmin Zheng <lianminzheng@gmail.com>
2022-06-09 00:20:51 +0000

    fix

--- tests/test_create_state.py
+++ tests/test_create_state.py
@@ -9,15 +9,16 @@ import jax.numpy as jnp
 import optax
 
 import alpa
-from alpa import init, parallelize, ShardParallel, CreateStateParallel
+from alpa import (init, parallelize, ShardParallel, PipeshardParallel,
+                  CreateStateParallel, manual_layer_construction)
 
 
 class CreateStateTest(unittest.TestCase):
     def setUp(self):
         init(cluster="ray")
 
-    def test_shard_parallel(self):
-        use_bias = False
+    def run_test(self, method):
+        use_bias = True
         batch_size = 8
         input_dim = output_dim = hidden_dim = 128
 
@@ -27,6 +28,8 @@ class CreateStateTest(unittest.TestCase):
             def __call__(self, x):
                 x = nn.Dense(features=hidden_dim, use_bias=use_bias)(x)
                 x = nn.Dense(features=hidden_dim, use_bias=use_bias)(x)
+                if isinstance(method, PipeshardParallel):
+                    alpa.mark_pipeline_boundary()
                 x = nn.Dense(features=hidden_dim, use_bias=use_bias)(x)
                 x = nn.Dense(features=output_dim, use_bias=use_bias)(x)
                 return x
@@ -36,6 +39,9 @@ class CreateStateTest(unittest.TestCase):
             def loss_func(params):
                 out = state.apply_fn(params, batch["x"])
                 return jnp.mean((out - batch["y"])**2)
+
+            if isinstance(method, PipeshardParallel):
+                loss_func = manual_layer_construction(loss_func)
 
             grads = alpa.grad(loss_func)(state.params)
             new_state = state.apply_gradients(grads=grads)
@@ -53,31 +59,41 @@ class CreateStateTest(unittest.TestCase):
             "y": jnp.ones((batch_size, output_dim)),
         }
 
-        # Test with and without gradient accumulation
-        for num_micro_batches in [None, 2]:
-            p_train_step = parallelize(train_step,
-                                     method=ShardParallel(num_micro_batches=num_micro_batches))
-            p_create_state = parallelize(create_state, method=CreateStateParallel(p_train_step, batch))
+        train_step = parallelize(train_step, method=method)
+        create_state = parallelize(create_state, method=CreateStateParallel(train_step, batch))
 
-            state = p_create_state()
-            state = p_train_step(state, batch)
+        state = create_state()
+        state = train_step(state, batch)
 
-            actual = p_create_state.get_last_executable().output_sharding_specs
+        actual = create_state.get_last_executable().output_sharding_specs
 
-            if num_micro_batches == None:
-                # Test NormalMeshDriverExecutable
-                expected = p_train_step.get_last_executable().input_sharding_specs[:len(actual)]
-            else:
-                # Test GradAccMeshDriverExecutable
-                expected = p_train_step.get_last_executable().global_arg_sharding_specs[:len(actual)]
+        if isinstance(method, ShardParallel):
+            if method.num_micro_batches == None: # NormalMeshDriverExecutable
+                expected = train_step.get_last_executable().input_sharding_specs[:len(actual)]
+            else: # GradAccMeshDriverExecutable
+                expected = train_step.get_last_executable().global_arg_sharding_specs[:len(actual)]
+        elif isinstance(method, PipeshardParallel):
+            pass
 
-            for x, y in zip(actual, expected):
-                assert x == y
+        for x, y in zip(actual, expected):
+            assert x == y
+
+    def test_shard_parallel(self):
+        method = ShardParallel(num_micro_batches=None)
+        self.run_test(method)
+
+        method = ShardParallel(num_micro_batches=2)
+        self.run_test(method)
+
+    def test_pipeshard_parallel(self):
+        method = PipeshardParallel(num_micro_batches=2)
+        self.run_test(method)
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(CreateStateTest("test_shard_parallel"))
+    #suite.addTest(CreateStateTest("test_pipeshard_parallel"))
     return suite
 
 

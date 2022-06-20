62278347b814d286dcc074a439d541b431620f5c
Will Feng (DPER) <willfeng@fb.com>
2022-06-20 16:28:42 -0700

    update

--- alpa/pipeline_parallel/pipeshard_executable.py
+++ alpa/pipeline_parallel/pipeshard_executable.py
@@ -459,17 +459,11 @@ class PipeshardMeshWorkerExecuable:
                 self.worker.delete_buffers(global_id)
         # load the local env
         self.worker.buffers = buffers
-        # sync_func = self.worker.sync if sync_for_timer else None
-        sync_func = self.worker.sync
+        sync_func = self.worker.sync if sync_for_timer else None
 
         # Execute
         timers("overall").start(sync_func=sync_func)
         for instruction in self.instructions:
-            # print("here1")
-            # sync_func()
-            # print("here2")
-            # print(f"before inst {instruction}")
-            # os.system("nvidia-smi")
             # print(f"memory_allocated: "
             #       f"{self.worker.get_memory_allocated()/1024**3:.3f} GB  "
             #       f"max_memory_allocated: "

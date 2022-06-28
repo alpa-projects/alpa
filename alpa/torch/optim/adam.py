14b39099d21d6ef864fca218e011440482db2ebe
Will Feng (DPER) <willfeng@fb.com>
2022-06-28 15:56:11 -0700

    update

--- alpa/torch/optim/adam.py
+++ alpa/torch/optim/adam.py
@@ -44,7 +44,7 @@ def adam(lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
                 exp_avg = exp_avg * beta1 + (1 - beta1) * grad
                 exp_avg_sq = exp_avg_sq * beta2 + (1 - beta2) * grad * grad.conj()
                 bias_correction2_sqrt = math.sqrt(bias_correction2)
-                denom = (torch.sqrt(exp_avg_sq) / torch.full_like(exp_avg_sq, bias_correction2_sqrt)) + torch.full_like(exp_avg_sq, eps)
+                denom = (torch.sqrt(exp_avg_sq) / torch.sqrt(torch.full_like(exp_avg_sq, bias_correction2))) + torch.full_like(exp_avg_sq, eps)
 
                 step_size = lr / bias_correction1
                 param = param + (-step_size * exp_avg / denom)

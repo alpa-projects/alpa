75882be6752c8d7a910a36e83bfa562cd7e43199
Will Feng (DPER) <willfeng@fb.com>
2022-06-28 10:53:32 -0700

    update

--- alpa/torch/optim/adam.py
+++ alpa/torch/optim/adam.py
@@ -54,15 +54,15 @@ def adam(lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
             return params, optim_state
 
         optim_state = {
-            "exp_avgs": {k: torch.empty_like(v, device="meta") for k, v in params},
-            "exp_avg_sqs": {k: torch.empty_like(v, device="meta") for k, v in params},
+            "exp_avgs": {k: torch.empty_like(v, device="meta") for k, v in params.items()},
+            "exp_avg_sqs": {k: torch.empty_like(v, device="meta") for k, v in params.items()},
             "step": torch.empty(1, device="meta"),
         }
 
         def optim_state_init_func(optim_state):
             new_state = {}
-            new_state["exp_avgs"] = {k: torch.zeros_like(v) for k, v in optim_state["exp_avgs"]}
-            new_state["exp_avg_sqs"] = {k: torch.zeros_like(v) for k, v in optim_state["exp_avg_sqs"]}
+            new_state["exp_avgs"] = {k: torch.zeros_like(v) for k, v in optim_state["exp_avgs"].items()}
+            new_state["exp_avg_sqs"] = {k: torch.zeros_like(v) for k, v in optim_state["exp_avg_sqs"].items()}
             new_state["step"] = torch.full_like(optim_state["step"], 1),
             return new_state
 

781ae77b95a069d3177a30b2d3aa198dac98be01
Will Feng (DPER) <willfeng@fb.com>
2022-06-29 20:39:31 -0700

    update

--- tests/torch_frontend/test_vit.py
+++ tests/torch_frontend/test_vit.py
@@ -252,18 +252,18 @@ params = {
 #     "mlp_d": 5120,
 # }
 
-# # ViT-10B model
-# arch_params = {
-#     "n_layers": 32,
-#     "n_heads": 32,
-#     "hidden_d": 5120,
-#     "mlp_d": 20480,
-# }
-# parallel_config = {
-#     "global_batch_size": 512,  # 512
-#     "num_micro_batches": 256,  # 128,
-#     "num_auto_layers": 16,
-# }
+# ViT-10B model
+arch_params = {
+    "n_layers": 32,
+    "n_heads": 32,
+    "hidden_d": 5120,
+    "mlp_d": 20480,
+}
+parallel_config = {
+    "global_batch_size": 512,  # 512
+    "num_micro_batches": 256,  # 128,
+    "num_auto_layers": 16,
+}
 
 # # ViT-25B model
 # arch_params = {
@@ -273,18 +273,18 @@ params = {
 #     "mlp_d": 30720,
 # }
 
-# ViT-120B model
-arch_params = {
-    "n_layers": 96,
-    "n_heads": 80,
-    "hidden_d": 10240,
-    "mlp_d": 40960,
-}
-parallel_config = {
-    "global_batch_size": 32,  # 512
-    "num_micro_batches": 32,  # 512,
-    "num_auto_layers": 64,
-}
+# # ViT-120B model
+# arch_params = {
+#     "n_layers": 96,
+#     "n_heads": 80,
+#     "hidden_d": 10240,
+#     "mlp_d": 40960,
+# }
+# parallel_config = {
+#     "global_batch_size": 32,  # 512
+#     "num_micro_batches": 32,  # 512,
+#     "num_auto_layers": 64,
+# }
 
 """
 32: get_solution_case("15B", num_micro_batches=128,

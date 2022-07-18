437537b280ea670683b59b01aea9baff74dafa35
Hao Zhang <sjtu.haozhang@gmail.com>
2022-07-17 19:12:35 -0700

    update with bs=4

--- examples/opt_serving/textgen_demo.py
+++ examples/opt_serving/textgen_demo.py
@@ -7,21 +7,25 @@ from opt_serving.model.wrapper import get_model
 tokenizer = AutoTokenizer.from_pretrained("facebook/opt-30b", use_fast=False)
 tokenizer.add_bos_token = False
 
-generate_params = {"do_sample": True, "num_beams": 4, "num_return_sequences": 3}
+generate_params = {"do_sample": False, "num_beams": 1, "num_return_sequences": 1}
 
 # Load the model
 model = get_model(model_name="alpa/opt-2.7b",
                   device="cuda",
                   path="/home/ubuntu/opt_weights",
+                  batch_size=4,
                   **generate_params)
 
 # Generate
-prompt = "Paris is the capital city of"
+prompt = ["Paris is the capital city of",
+          "Today is a good day and I'd like to",
+          "Computer Science studies the area of",
+          "University of California Berkeley is a public university"]
+# prompt = "Paris is the capital city of"
 
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+input_ids = tokenizer(prompt, return_tensors="pt", padding="longest").input_ids.to("cuda")
 outputs = model.generate(input_ids=input_ids,
-                         max_length=256,
-                         no_repeat_ngram_size=2,
+                         max_length=32,
                          **generate_params)
 
 # Print results

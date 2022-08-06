0d52805cf1f7156767d4bcf486ec8021d652c15f
Will Feng (DPER) <willfeng@fb.com>
2022-08-05 19:56:06 -0700

    fix bug

--- docker/scripts/install_torch.sh
+++ docker/scripts/install_torch.sh
@@ -5,7 +5,7 @@ install_torch_deps() {
     # NOTE: functorch is pinned to the last commit that works with PyTorch 1.12
     pip install --extra-index-url https://download.pytorch.org/whl/cpu torch==1.12 torchdistx && \
     ([ -d "functorch" ] || git clone https://github.com/pytorch/functorch) && \
-    pushd functorch && git checkout 091d9999b16bf0015b735971580be2d9ad704144 && python setup.py install && popd
+    pushd functorch && git checkout 76976db8412b60d322c680a5822116ba6f2f762a && python setup.py install && popd
 }
 
 install_torch_deps

e19bbf4a2e51a9f1c0953a3c0fb817033efe50b9
Lianmin Zheng <lianminzheng@gmail.com>
2022-08-16 04:03:40 -0700

    Update alpa/device_mesh.py

--- alpa/device_mesh.py
+++ alpa/device_mesh.py
@@ -934,7 +934,7 @@ class DistributedPhysicalDeviceMesh(PhysicalDeviceMesh):
 
         self.devices = devices
         self.device_strs = []
-        self.device_ips = []
+        self.node_ips = []
         for i in range(self.num_hosts):
             ip = self.host_info[i]["NodeManagerAddress"]
             self.device_strs.extend(

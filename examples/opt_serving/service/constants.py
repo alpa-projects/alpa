670debb8530b28ab6c81078f5a732f184acaf634
Lianmin Zheng <lianminzheng@gmail.com>
2022-08-23 11:58:28 +0000

    clean up host

--- examples/opt_serving/service/constants.py
+++ examples/opt_serving/service/constants.py
@@ -2,10 +2,15 @@
 
 MAX_SEQ_LEN = 512
 MAX_BATCH_TOKENS = 3072
-TIMEOUT_MS = 2000
+BATCHING_TIMEOUT_MS = 2000
 LOGDIR = "weblogs"
 MAX_BS = 8
 
 # Generation-related params
 NUM_BEAMS = 1
 NUM_RETURN_SEQ = 1
+
+# Logprobs endpoint cache-related params
+LOGPROBS_BATCHING_TIMEOUT_MS = 0
+LOGPROBS_PAST_CACHE_TIMEOUT = 60 * 10 # 10 minutes
+LOGPROBS_PAST_CACHE_SIZE_LIMIT = 20 # max sets of past_key_values to keep at a time in cache

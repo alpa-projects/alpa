7baef786b38ce7dabc259dd7d8795e75e9b9cb7c
Zhuohan Li <zhuohan123@gmail.com>
2022-08-28 16:32:39 -0700

    format

--- alpa/pipeline_parallel/stage_profiling.py
+++ alpa/pipeline_parallel/stage_profiling.py
@@ -779,8 +779,9 @@ def get_compute_cost(
 
         stages = generate_2d_training_stages(
             layers, layer_flops_prefix_sum, donation_mapping, global_outvars,
-            apply_grad_layers, apply_grad_global_info, autosharding_configs[mesh_id],
-            mesh_is_profiled, sliced_virtual_meshes[0].num_devices, cluster_size,
+            apply_grad_layers, apply_grad_global_info,
+            autosharding_configs[mesh_id], mesh_is_profiled,
+            sliced_virtual_meshes[0].num_devices, cluster_size,
             auto_stage_option.stage_imbalance_tolerance)
         (mesh_compute_cost,
          mesh_max_n_succ_stages, mesh_profiled) = distributed_profile_on_mesh(

610d5ab0c933b411706e73d8ea1494f1bf8f4713
Lianmin Zheng <lianminzheng@gmail.com>
2022-09-02 23:22:40 +0000

    remove custom call marker

--- alpa/pipeline_parallel/xla_custom_call_marker/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p build
-cd build
-cmake .. -DPYTHON_EXECUTABLE=$(which python3) -Dpybind11_ROOT=$(pybind11-config --cmakedir)
-# Set `CUDA_HOME` to /usr/local/cuda only if it's not already set
-export CUDA_HOME="${CUDA_HOME:=/usr/local/cuda}"
-export PATH=$CUDA_HOME/bin:$PATH
-export CPATH=$CUDA_HOME/targets/x86_64-linux/include/:$CPATH
-make
--- alpa/pipeline_parallel/xla_custom_call_marker/kernel.h
+++
@@ -1,7 +0,0 @@
-#include <cuda_runtime_api.h>
-
-namespace kernel {
-
-void identity(cudaStream_t stream, void **buffers, const char *opaque, size_t opaque_len);
-
-}  // namespace kernel

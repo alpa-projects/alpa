683e4622ecffe10c24c785fb33246ef3e515b183
Hao Zhang <zhisbug@gmail.com>
2022-09-07 15:38:00 -0400

    update benchmarks

--- examples/opt_serving/benchmark/benchmark_1d.py
+++ examples/opt_serving/benchmark/benchmark_1d.py
@@ -1,7 +1,8 @@
 import argparse
 import copy
 import time
-from examples.opt_serving.model.test_1d import setup
+from examples.opt_serving.model.test_1d import setup, runner_1d, runner_2d
+import jax.numpy as jnp
 from examples.opt_serving.benchmark.benchmark_text_gen import test_prompts
 from alpa.util import print_used_time
 
@@ -11,6 +12,11 @@ input_id_list = [
     [133, 589, 9, 886, 6, 10817, 16, 10, 285],
     [5625, 16, 10, 205, 183, 8, 38, 236, 7],
     [2264, 16, 5, 7440, 9, 16673, 873, 24214, 116],
+    # [32826, 16, 5, 812, 343, 9],
+    # [2264, 109, 47, 206, 59, 5, 499, 9, 28850, 1975, 37079, 116],
+    # [2264, 109, 47, 206, 59, 5, 3099, 9, 301, 116],
+    # [19195, 140, 16, 5, 394, 9],
+    # [534, 10311, 12, 246, 16, 10, 739, 2777, 1421, 14, 16, 4453, 9],
 ]
 
 
@@ -25,42 +31,32 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="jax/opt-125m")
     parser.add_argument("--path", type=str, default="~/opt_weights/")
-    # parser.add_argument("--dummy", action="store_true")
-    # parser.add_argument("--forward", action="store_true")
-    # parser.add_argument("--forward-encoder-length", type=int, default=1024)
-    # parser.add_argument("--nb", type=int, default=1)
-    # parser.add_argument("--batch-size", type=int, default=1)
     parser.add_argument("--n-warmup", type=int, default=1)
-    # parser.add_argument("--n-iter", type=int, default=10)
-    # parser.add_argument("--max-length", type=int, default=256)
-    # parser.add_argument("--pad-to-max-length", type=int)
-    # parser.add_argument("--num-beams", type=int, default=1)
-    # parser.add_argument("--debug", action="store_true")
-    # parser.add_argument("--dtype", type=str, default="fp16")
 
     args = parser.parse_args()
-    runner_1d, input_pool_1d = setup("1d", input_id_list, np_weights_folder=args.path)
-    runner_2d, input_pool_2d = setup("2d", input_id_list, np_weights_folder=args.path)
+    model_1d, input_pool_1d = setup("1d", input_id_list, np_weights_folder=args.path)
+    model_2d, input_pool_2d = setup("2d", input_id_list, np_weights_folder=args.path)
 
     # Warm up
     for i in range(args.n_warmup):
         warmup_pool_2d = copy.deepcopy(input_pool_2d)
-        _, cost = runner_2d(warmup_pool_2d[2:])
+        _, cost = runner_2d(model_2d, warmup_pool_2d)
         print_execution_cost(cost)
     for i in range(args.n_warmup):
         warmup_pool_1d = copy.deepcopy(input_pool_1d)
-        _, cost = runner_1d(warmup_pool_1d[2:])
+        warmup_pool_1d.kv_caches = [(jnp.asarray(k), jnp.asarray(v)) for k, v in warmup_pool_1d.kv_caches]
+        _, cost = runner_1d(model_1d, warmup_pool_1d)
         print_execution_cost(cost)
     print(" === Warm up done! ===")
 
     # benchmark prompt
     print("Run 1D")
     tic = time.time()
-    ret_pool_1d, _ = runner_1d(input_pool_1d[2:])
+    ret_pool_1d, _ = runner_1d(model_1d, input_pool_1d)
     latency_1d = time.time() - tic
     print("Run 2D")
     tic = time.time()
-    ret_pool_2d, _ = runner_2d(input_pool_2d[2:])
+    ret_pool_2d, _ = runner_2d(model_2d, input_pool_2d)
     latency_2d = time.time() - tic
     print(f"Results 3 prompts: 1d {latency_1d}, 2d {latency_2d}")
     exit(1)
--- examples/opt_serving/model/test_1d.py
+++ examples/opt_serving/model/test_1d.py
@@ -57,7 +57,12 @@ def record_time(start=False):
 
 
 class Jax1DInput:
-    def __init__(self, input_tokens=None, input_sentence_ids=None, kv_caches=None, kv_cache_ids=None, num_prev_tokens=None):
+    def __init__(self,
+                 input_tokens,
+                 input_sentence_ids,
+                 kv_caches,
+                 kv_cache_ids,
+                 num_prev_tokens):
         self.input_tokens = input_tokens
         self.input_sentence_ids = input_sentence_ids
         self.kv_caches = kv_caches
@@ -130,8 +135,8 @@ def runner_2d(model, input_pool):
     return input_pool, execution_cost
 
 
-def setup(mode,
-          input_id_list,
+def setup(mode: str,
+          input_id_list: List[List[int]],
           model="jax/opt-125m",
           np_weights_folder="~/opt_weights/"):
     name = model.split("-")[1].upper()
@@ -144,12 +149,6 @@ def setup(mode,
         assert mode == "1d", "only support `1d` or `2d`."
         inference_step, params, kv_caches, kv_caches_cupy, config = init_1d_inference_step(name, path)
         model_tuple = tuple([inference_step, params, config])
-        # make input pool for 1D
-        # input_tokens = input_id_list
-        # input_sentence_ids = np.arange(1, len(input_tokens) + 1)
-        # kv_cache_ids = np.zeros((M, ), dtype=np.int32)
-        # num_prev_tokens: Dict[int, int] = {}
-        # input_pool = tuple([input_tokens, input_sentence_ids, kv_caches, kv_caches_cupy, kv_cache_ids, num_prev_tokens])
         input_pool = Jax1DInput(input_id_list,
                                 [i for i in range(1, len(input_id_list) + 1)],
                                 kv_caches,
@@ -182,7 +181,7 @@ def init_1d_inference_step(name, np_weights_folder, total_input_len=N, total_cac
 
 
 def runner_1d(model, input_pool):
-    # input_tokens should not include paddings.
+    execution_cost = {}
 
     inference_step, params, config = model
     input_tokens = input_pool.input_tokens
@@ -192,13 +191,12 @@ def runner_1d(model, input_pool):
     num_prev_tokens = input_pool.num_prev_tokens
 
     kv_caches_cupy = [(jax_tensor_to_cupy(k), jax_tensor_to_cupy(v)) for k, v in kv_caches]
-    # input_tokens, input_sentence_ids, kv_caches, kv_caches_cupy, kv_cache_ids, num_prev_tokens = input_pool
-
+    # input_tokens should not include paddings.
     for input_sentence in input_tokens:
         assert config.pad not in input_sentence
 
     # TODO(Woosuk): Reorder the input sentences.
-
+    record_time(start=True)
     # Pre-update num_prev_tokens.
     for i, sentence_id in enumerate(input_sentence_ids):
         if sentence_id in num_prev_tokens:
@@ -229,19 +227,24 @@ def runner_1d(model, input_pool):
     position_id_1d = position_id_1d + [config.pad] * (N - len(position_id_1d))
     position_id_1d = jnp.asarray(position_id_1d, dtype=jnp.int32)
 
+    execution_cost["prep"] = record_time()
+
     assert MAX_CACHE_LEN_PER_SEQ >= max(num_prev_tokens.values())
     assert MAX_CACHE_LEN_PER_SEQ * len(num_prev_tokens) <= kv_cache_ids.shape[0]
     os.environ['FT_INPUT_INDEX_ADDR'] = str(input_index_1d.ctypes.data)
     os.environ['FT_CACHE_INDEX_ADDR'] = str(kv_cache_ids.ctypes.data)
     os.environ['FT_MAX_CACHE_LEN_PER_SEQ'] = str(MAX_CACHE_LEN_PER_SEQ)
 
+    execution_cost["set env"] = record_time()
+
     batch = {
         "input_ids": input_1d,
         "position_ids": position_id_1d,
         "cache": kv_caches
     }
-
     logits, kv = inference_step(params, batch)
+
+    execution_cost["compute"] = record_time()
 
     # Get the output tokens.
     logits = np.array(logits)
@@ -272,221 +275,12 @@ def runner_1d(model, input_pool):
         # TODO: Handle EOS here.
         num_prev_tokens[sentence_id] += len(input_tokens[i])
 
-    kv_caches = [(cupy_to_jax_tensor(k), cupy_to_jax_tensor(v)) for k, v in kv_caches_cupy]
-    # TODO(Hao): make outputs as an input_pool
+    execution_cost["cache reorder"] = record_time()
+
+    # kv_caches = [(cupy_to_jax_tensor(k), cupy_to_jax_tensor(v)) for k, v in kv_caches_cupy]
     output_pool = Jax1DInput(outputs, input_sentence_ids, kv_caches, kv_cache_ids, num_prev_tokens)
-    return output_pool
+    return output_pool, execution_cost
 
-# def runner_1d(input_pool, inference_step, params, config):
-#     batch_size = len(input_pool)
-#     is_prompt = [cache[0][2][0] == 0 for _, cache in input_pool]
-#     execution_cost = {}
-#
-#     record_time(start=True)
-#     # Concat inputs together
-#     input_ids_flatten = []
-#     position_ids_flatten = []
-#     batch_idxs = []
-#
-#     # Prompts must go first, so we use this flag to make sure
-#     # input pool does not have a pattern like [prompt1, token1, prompt2].
-#     in_prompt = True
-#     for idx, (input_ids, cache) in enumerate(input_pool):
-#         position_ids = opt_model_1d.build_position_ids(
-#             np.array(input_ids, dtype=np.int32), config.pad).tolist()
-#
-#         if not is_prompt[idx]:
-#             # Auto-regressive
-#             input_ids = input_ids[-1:]
-#             position_ids = position_ids[-1:]
-#             in_prompt = False
-#         else:
-#             assert in_prompt, "Prompts must be consecutive and before tokens"
-#         input_ids_flatten += input_ids
-#         position_ids_flatten += position_ids
-#
-#         # fused_mmha kernel requires batch_idxs to be in shape sum(input_ids). Each element
-#         # in batch_idxs is the ID of the corresponding input sequence (starting from 1).
-#         batch_idxs += np.full(shape=(len(input_ids),),
-#                               fill_value=idx + 1,
-#                               dtype=np.int32).tolist()
-#
-#     input_ids_flatten = np.array(input_ids_flatten, dtype=np.int32)
-#     position_ids_flatten = np.array(position_ids_flatten, dtype=np.int32)
-#     batch_idxs = np.array(batch_idxs, dtype=np.int32)
-#     execution_cost["flattening"] = record_time()
-#
-#     # Concate per-input cache together and generate cache index.
-#     # Note that assuming the valid cache length of 3 inputs are L1, L2, L3,
-#     # and the maximum length is L2, then the cache is organized as follows:
-#     # [<1xL1>, <0x(L2-L1+1)>, <1xL2>, <0x1>, <1xL3>, <0x(L2-L3+1)>, 0, ...]
-#     # And the total length is BxP, where B is the batch size and P is the
-#     # maximum cache length. Here we set P to be the max_target_position.
-#     target_cache_len = max([cache[0][2][0] for _, cache in input_pool]) + 1
-#     cache_1d_flatten = []
-#     head_dim = config.decoder_embed_dim // config.decoder_attention_heads
-#     for batched_layer_cache in zip(*[cache for _, cache in input_pool]):
-#         batched_layer_key = []
-#         batched_layer_value = []
-#         batched_layer_index = []
-#         for bidx, (key, value, index) in enumerate(batched_layer_cache):
-#             if is_prompt[bidx]:
-#                 continue
-#             curr_valid_len = index[0]
-#             batched_layer_key.append(key[:target_cache_len])
-#             batched_layer_value.append(value[:target_cache_len])
-#
-#             seq_ids = np.full(
-#                 (curr_valid_len,),  # Valid cache length
-#                 bidx + 1,  # Sequence ID starting from 1
-#                 dtype=np.int32)
-#             batched_layer_index.append(
-#                 np.concatenate([
-#                     seq_ids,
-#                     np.zeros((target_cache_len - curr_valid_len,),
-#                              dtype=np.int32)
-#                 ]))
-#
-#         if batched_layer_key:
-#             key_flatten = np.concatenate(batched_layer_key, axis=0)
-#             value_flatten = np.concatenate(batched_layer_value, axis=0)
-#             index_flatten = np.concatenate(batched_layer_index, axis=0)
-#         else:
-#             # In the case of all prompts we just put empty cache and pad all 0s.
-#             key_flatten = np.empty(
-#                 (0, config.decoder_attention_heads, head_dim),
-#                 dtype=np.float32)
-#             value_flatten = np.empty(
-#                 (0, config.decoder_attention_heads, head_dim),
-#                 dtype=np.float32)
-#             index_flatten = np.empty((0,), dtype=np.int32)
-#
-#         # Pad 0s to fixed length: batch_size * max_target_positions
-#         pad_len = batch_size * config.max_target_positions - index_flatten.shape[
-#             0]
-#         key_val_pad = np.zeros((pad_len,) + key_flatten.shape[1:],
-#                                dtype=np.float32)
-#         key_flatten = np.concatenate([key_flatten, key_val_pad], axis=0)
-#         value_flatten = np.concatenate([value_flatten, key_val_pad], axis=0)
-#
-#         index_pad = np.zeros((pad_len,), dtype=np.int32)
-#         index_flatten = np.concatenate([index_flatten, index_pad], axis=0)
-#         cache_1d_flatten.append((key_flatten, value_flatten, index_flatten))
-#
-#     execution_cost["cache"] = record_time()
-#     # FIXME(Hao): Set env var for this workaround:
-#     #  https://github.com/WoosukKwon/1d-mmha-op/blob/main/lib/kernels.cc.cu#L53
-#     host_device = jax.devices('cpu')[0]
-#     host_input_index = jax.device_put(batch_idxs, device=host_device)
-#     host_cache_index = jax.device_put(cache_1d_flatten[0][2], device=host_device)
-#     os.environ["FT_INPUT_INDEX_ADDR"] = str(host_input_index.unsafe_buffer_pointer())
-#     os.environ["FT_CACHE_INDEX_ADDR"] = str(host_cache_index.unsafe_buffer_pointer())
-#     execution_cost["set_env"] = record_time()
-#
-#     logits, cache_1d_updated = inference_step(
-#         params, {
-#             "input_ids": input_ids_flatten,
-#             "position_ids": position_ids_flatten,
-#             "batch_idxs": batch_idxs,
-#             "cache": cache_1d_flatten,
-#         })
-#     execution_cost["compute"] = record_time()
-#     if DUMP_LOGITS:
-#         jnp.save("1d", logits)
-#
-#     updated_lengths = [
-#         len(ids) if prompt else 1
-#         for prompt, (ids, _) in zip(is_prompt, input_pool)
-#     ]
-#
-#     # Recover and update the cache.
-#     # The output cache only includes the new values of the current generated token,
-#     # so we perform dynamic slice update on the original cache.
-#     for lidx, (key_flatten, value_flatten) in enumerate(cache_1d_updated):
-#         update_start = 0
-#         key_flatten = key_flatten.squeeze()
-#         value_flatten = value_flatten.squeeze()
-#         for bidx in range(batch_size):
-#             cache = input_pool[bidx][1]
-#             curr_index = cache[lidx][2][0]
-#
-#             # Determine the length of updated cache.
-#             update_end = update_start + updated_lengths[bidx]
-#             new_index = curr_index + updated_lengths[bidx]
-#
-#             # Slice update.
-#             cache[lidx][0][curr_index:new_index] = \
-#                 key_flatten[update_start:update_end]
-#             cache[lidx][1][curr_index:new_index] = \
-#                 value_flatten[update_start:update_end]
-#             cache[lidx][2][0] = new_index
-#             input_pool[bidx] = (input_pool[bidx][0], cache)
-#
-#             update_start = update_end
-#
-#     # Append the generated token.
-#     update_start = 0
-#     update_end = 0
-#     for bidx, (_input_ids, cache) in enumerate(input_pool):
-#         update_end = update_start + updated_lengths[bidx]
-#         next_token = np.argmax(logits[update_start:update_end], axis=-1)[-1]
-#         input_pool[bidx] = (_input_ids + [next_token.tolist()], cache)
-#         update_start = update_end
-#     assert update_end == logits.shape[0]
-#     execution_cost["reorder"] = record_time()
-#
-#     return input_pool, execution_cost
-
-# def init_1d_runner(name, np_weights_folder, input_id_list):
-#     inference_step, params, cache, config = init_1d_inference_step(name, np_weights_folder)
-#
-#     # Make cache for each input
-#     input_pool_1d = [(input_id, copy.deepcopy(cache))
-#                      for input_id in input_id_list]
-#
-#     return runner, input_pool_1d
-
-
-def verify_status(result_1d, result_2d):
-    """Verify the current sequence and cache values."""
-    print("Verifying results...", flush=True, end="")
-    success = True
-
-    tokens_1d = result_1d.input_tokens
-    caches_1d = result_1d.kv_caches
-
-    new_results_1d = [(tokens, caches_1d[i]) for i, tokens in enumerate(tokens_1d)]
-
-    for bidx, ((seq_1d, cache_1d),
-               (seq_2d, cache_2d)) in enumerate(zip(new_results_1d, result_2d)):
-        # Verify sequence.
-        try:
-            assert_allclose(seq_1d, seq_2d)
-        except AssertionError as err:
-            print("Result of seq %d does not match: %s" % (bidx, str(err)))
-            success = False
-
-        # Verify cache.
-        for lidx, (layer_2d, layer_1d) in enumerate(zip(cache_2d, cache_1d)):
-            # Layer
-            assert len(layer_2d) == len(layer_1d), \
-                "KV length mismatch: %d vs. %d" % (len(layer_2d), len(layer_1d))
-
-            key_1d, value_1d, _ = layer_1d
-            key_2d, value_2d, _ = layer_2d
-            key_2d = key_2d.reshape(key_1d.shape)
-            value_2d = value_2d.reshape(value_1d.shape)
-            for cidx, (col_2d, col_1d) in enumerate(zip(key_2d, key_1d)):
-                try:
-                    assert_allclose(col_2d, col_1d)
-                except AssertionError as err:
-                    print("KV value mismatch for seq %d at layer %d column %d" %
-                          (bidx, lidx, cidx))
-                    print(str(err))
-                    success = False
-    if not success:
-        raise RuntimeError("Failed")
-    print("passed", flush=True)
 
 def verify_next_token(output_pool_1d, output_pool_2d):
     print("Verifying next token prediction...", flush=True, end="")
@@ -503,6 +297,7 @@ def verify_next_token(output_pool_1d, output_pool_2d):
     if not success:
         raise RuntimeError("Failed")
     print("passed", flush=True)
+
 
 def verify_caches(output_pool_1d, output_pool_2d):
     print("Verifying caches...", flush=True, end="")
@@ -560,58 +355,6 @@ def verify_caches(output_pool_1d, output_pool_2d):
     print("passed", flush=True)
 
 
-def simulate_serving(runner_n_pool, ref_runner_n_pool=None):
-    """Simulate model serving."""
-
-    def batch_first_N_prompt(runner, prompt_pool, msg_prefix=""):
-        """Batch 1 (prompt * 3)."""
-        ret = runner(prompt_pool[2:])
-        print("%s Batch 1 (prompt * 3) done" % msg_prefix, flush=True)
-        return ret
-
-    def batch_mixed(runner, prompt_pool, decode_pool, msg_prefix=""):
-        """Batch 2 (prompt * 2, decoding * 3)."""
-        ret = runner(prompt_pool[:2] + decode_pool)
-        print("%s Batch 2 (prompt * 2, decoding * 3) done" % msg_prefix,
-              flush=True)
-        return ret
-
-    def batch_all_decode(runner, decode_pool, msg_prefix="", batch_idx=3):
-        """Batch 3+ (decoding * 5)."""
-        ret = runner(decode_pool)
-        print("%s Batch %d (decoding * 5) done" % (msg_prefix, batch_idx),
-              flush=True)
-        return ret
-
-    runner, input_pool = runner_n_pool
-
-    verify = False
-    ref_runner, ref_input_pool = None, None
-    if ref_runner_n_pool is not None:
-        verify = True
-        ref_runner, ref_input_pool = ref_runner_n_pool
-
-    decode_pool, _ = batch_first_N_prompt(runner, input_pool, "1D")
-    if verify:
-        ref_decode_pool, _ = batch_first_N_prompt(ref_runner, ref_input_pool, "2D")
-        verify_status(decode_pool, ref_decode_pool)
-
-    decode_pool, _ = batch_mixed(runner, input_pool, decode_pool, "1D")
-    if verify:
-        ref_decode_pool, _ = batch_mixed(ref_runner, ref_input_pool,
-                                      ref_decode_pool, "2D")
-        verify_status(decode_pool, ref_decode_pool)
-
-    for idx in range(5):
-        batch_idx = idx + 3
-        decode_pool, _ = batch_all_decode(runner, decode_pool, "1D", batch_idx)
-        if verify:
-            ref_decode_pool, _ = batch_all_decode(ref_runner, ref_decode_pool,
-                                               "2D", batch_idx)
-            verify_status(decode_pool, ref_decode_pool)
-
-
-
 def test_opt_125M():
     name = "jax/opt-125m"
     np_weights_folder = f"/home/ubuntu/opt_weights/"
@@ -624,12 +367,13 @@ def test_opt_125M():
 
     # batch the first 5 prompts
     # input_tokens, input_sentence_ids, kv_caches, kv_caches_cupy, kv_cache_ids, num_prev_tokens
-    output_pool_1d = runner_1d(model_1d, input_pool_1d[:batch_size])
+    output_pool_1d, _ = runner_1d(model_1d, input_pool_1d[:batch_size])
     output_pool_2d, _ = runner_2d(model_2d, input_pool_2d[:batch_size])
     verify_next_token(output_pool_1d, output_pool_2d)
     verify_caches(output_pool_1d, output_pool_2d)
 
     # batch the second 5 prompts and the first 5 words
+    # Note(Hao): prompts must go first.
     output_pool_1d = Jax1DInput(
         input_id_list[batch_size:] + output_pool_1d.input_tokens,
         [i+1 for i in range(batch_size, len(input_id_list))] + output_pool_1d.input_sentence_ids,
@@ -639,18 +383,9 @@ def test_opt_125M():
     )
     output_pool_2d = input_pool_2d[batch_size:] + output_pool_2d
     output_pool_2d, _ = runner_2d(model_2d, output_pool_2d)
-    output_pool_1d = runner_1d(model_1d, output_pool_1d)
-
+    output_pool_1d, _ = runner_1d(model_1d, output_pool_1d)
     verify_next_token(output_pool_1d, output_pool_2d)
     verify_caches(output_pool_1d, output_pool_2d)
-
-
-    exit(1)
-    # TODO(Hao): do something with the pool
-    # batch all decode
-    output_pool_1d = runner_1d(output_pool_1d)
-    output_pool_2d = runner_2d(output_pool_2d)
-    verify_status(output_pool_1d, output_pool_2d)
 
 
 if __name__ == "__main__":

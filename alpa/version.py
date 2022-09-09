2a8539275bdb9a53414b3dbeedc49f5282390678
Lianmin Zheng <lianminzheng@gmail.com>
2022-09-09 00:36:47 +0000

    update

--- alpa/version.py
+++ alpa/version.py
@@ -21,7 +21,8 @@ if alpa_jaxlib_version < minimal_alpa_jaxlib_version:
     raise RuntimeError(
         f"The alpa-jaxlib's internal version is v{alpa_jaxlib_version_str}, "
         f"but the minimal requirement is v{minimal_alpa_jaxlib_version_str}. "
-        f"Please update your tensorflow-alpa submodule and re-compile jaxlib. "
+        f"Please install the latest alpa-jaxlib. If you build alpa from source,"
+        f" please update your tensorflow-alpa submodule and re-compile jaxlib. "
         f"Help : https://alpa-projects.github.io/developer/developer_guide.html"
         f"#updating-submodule-tensorflow-alpa.")
 

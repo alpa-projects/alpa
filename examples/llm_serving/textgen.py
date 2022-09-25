fd50e5819ad9a5f8d724a8c08f3d49c080e55ce6
Hao Zhang <zhisbug@gmail.com>
2022-09-25 19:49:26 -0400

    roll back [skip ci]

--- examples/llm_serving/textgen.py
+++ examples/llm_serving/textgen.py
@@ -1,13 +1,10 @@
 """Use huggingface/transformers interface and Alpa backend for distributed inference."""
 import argparse
-import time
 
-import jax
 import numpy as np
 from transformers import AutoTokenizer
 
 from llm_serving.model.wrapper import get_model
-from alpa.timer import timers
 
 def main(args):
     # Load the tokenizer.
@@ -31,53 +28,36 @@ def main(args):
     model = get_model(model_name=args.model,
                       path="~/opt_weights",
                       batch_size=args.n_prompts,
-                      encoder_chunk_sizes=(1, 16),
                       **generate_params)
 
     # Generate
     prompts = [
-        "Computer science is the study of computation and",
-        "Ion Stoica is a Romanian-American computer scientist specializing in",
-        "The University of California, Berkeley is a public",
-        "Today is a good day and I want to", "What is the valuation of Databricks?",
-        "Paris is the capital city of", "Which country has the most population?",
-        "What do you think about the future of Cryptocurrency?",
-        "What do you think about the meaning of life?",
-        "Donald Trump is the president of",
-        "GPT-3 is a large language model that is capable of"
+        "Paris is the capital city of",
+        "Today is a good day and I'd like to",
+        "Computer Science studies the area of",
+        "University of California Berkeley is a public university"
     ]
-
-    timer_names = ["enter", "compute", "move logits"]
     prompts = prompts[:args.n_prompts]
     input_ids = tokenizer(prompts, return_tensors="pt", padding="longest").input_ids
-    for i in range(10):
-        tic = time.time()
-        output_ids = model.generate(input_ids=input_ids,
-                                    max_length=64,
-                                    **generate_params)
-        elapsed = time.time() - tic
-        for timer_name in timer_names:
-            timers(timer_name).stop()
-        print(f"- It takes {elapsed}")
-        timers.log(timer_names)
-        for timer_name in timer_names:
-            timers(timer_name).reset()
-
-        outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-        if False:
-            print("Outputs:\n" + 100 * '-')
-            for i, output in enumerate(outputs):
-                print(f"{i}: {output}")
-                print(100 * '-')
+    output_ids = model.generate(input_ids=input_ids,
+                                max_length=64,
+                                **generate_params)
+    outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    
+    # Print results
+    print("Outputs:\n" + 100 * '-')
+    for i, output in enumerate(outputs):
+        print(f"{i}: {output}")
+        print(100 * '-')
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="jax/opt-125m")
+    parser.add_argument("--model", type=str, default="alpa/opt-1.3b")
     parser.add_argument('--do-sample', action='store_true')
     parser.add_argument('--num-beams', type=int, default=1)
     parser.add_argument('--num-return-sequences', type=int, default=1)
-    parser.add_argument('--n-prompts', type=int, default=10)
+    parser.add_argument('--n-prompts', type=int, default=4)
     args = parser.parse_args()
 
     main(args)

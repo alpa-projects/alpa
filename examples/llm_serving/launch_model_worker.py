a38bfde29e2c1ece5faf5bc59cc4189dde852091
Zhanyuan Zhang <32000378+zhanyuanucb@users.noreply.github.com>
2022-10-20 16:56:55 -0700

    [Serving] Handle x forwarded for header (#756)

--- examples/llm_serving/launch_model_worker.py
+++ examples/llm_serving/launch_model_worker.py
@@ -334,7 +334,11 @@ class LangaugeModelWorker:
         for x in request.scope['headers']:
             if x[0] == b"x-forwarded-for":
                 v = x[1].decode()
-                return v[:v.index(':')]
+                v = v.split(",")[0] # Obtain the client IP
+                if ":" in v:
+                    # Drop the port number
+                    return v[:v.index(":")]
+                return v
         return request.client.host
 
 

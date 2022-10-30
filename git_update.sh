57e6def9c27e2e38bea0ebcd5c5d3752db40f205
tntnnlrw <tntnnlrw@163.com>
2022-10-30 10:33:25 +0000

    update

--- git_update.sh
+++ git_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
-git --global user.email "tntnnlrw@163.com"
-git --global user.name "tntnnlrw"
+# git --global user.email "tntnnlrw@163.com"
+# git --global user.name "tntnnlrw"
 git add *
 git commit -m "update"
 git push

8a6225756dba7c91f696c76585025e0b9e919e5e
flyingpig <60659728+PKUFlyingPig@users.noreply.github.com>
2022-12-03 15:45:41 +0800

    [BENCHMARK] Profile inference stage latency/memory for MoE (#791)

--- benchmark/alpa/gen_serving_database.py
+++ benchmark/alpa/gen_serving_database.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, default="benchmark_results.tsv")
     parser.add_argument("--output", type=str, default="profiling_result.pkl")
+    parser.add_argument("--new", action="store_true")
     args = parser.parse_args()
 
-    database = ProfilingDatabase(args.output, True)
+    database = ProfilingDatabase(args.output, args.new)
     database.update_from_csv(args.input)
     database.materialize()

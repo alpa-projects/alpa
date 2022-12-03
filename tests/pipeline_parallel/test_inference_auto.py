bf8cf0c97258c19c491c579ad8f1d5993dfe25dd
Zhuohan Li <zhuohan123@gmail.com>
2022-12-03 01:07:42 -0800

    add bert tests

--- tests/pipeline_parallel/test_inference_auto.py
+++ tests/pipeline_parallel/test_inference_auto.py
@@ -42,12 +42,25 @@ class PipelineInferenceAutoTest(PipelineInferenceTest):
                                    stage_option=stage_option)
         self.run_mlp_inference(True, method)
 
+    def test_bert_1d(self):
+        stage_option = AutoStageOption(
+            submesh_physical_shape_space="manual",
+            manually_specified_submeshes=((1, 2),),
+            submesh_logical_shape_space="model_parallel_only",
+            layer_profile_mode="individual")
+        method = PipeshardParallel(num_micro_batches=1,
+                                   pipeline_schedule="inference",
+                                   layer_option="manual",
+                                   stage_option=stage_option)
+        self.run_bert_layer_collection_inference(True, method)
+
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(PipelineInferenceAutoTest("test_mlp"))
     suite.addTest(PipelineInferenceAutoTest("test_bert"))
     suite.addTest(PipelineInferenceAutoTest("test_mlp_1d"))
+    suite.addTest(PipelineInferenceAutoTest("test_bert_1d"))
     return suite
 
 

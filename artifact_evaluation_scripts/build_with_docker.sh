4e86dc7cf3ed6e1652b5691bc07e99e372c6d74c
ZYHowell <yhzhuang@cmu.ed>
2023-04-01 05:19:24 +0400

    update option to build with docker

--- artifact_evaluation_scripts/build_with_docker.sh
+++ artifact_evaluation_scripts/build_with_docker.sh
@@ -8,4 +8,6 @@ docker run --gpus all --tmpfs /build:exec \
   --rm -v $(pwd)/dist:/dist build-jaxlib-image \
   3.8 cuda 11.1 main master
 
-
+# Install the built jaxlib by:
+# cd dist
+# pip install jaxlib-0.3.22+cuda111.cudnn805-cp38-cp38-manylinux2014_x86_64

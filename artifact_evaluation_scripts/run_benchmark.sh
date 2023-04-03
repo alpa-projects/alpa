7e8025190ebadaba2628803b47f839577b40adcc
ZYHowell <yhzhuang@cmu.ed>
2023-04-03 05:12:35 +0400

    fix a typo

--- artifact_evaluation_scripts/run_benchmark.sh
+++ artifact_evaluation_scripts/run_benchmark.sh
@@ -1,5 +1,5 @@
-python3 microbenchmark.py --1-to-m --functional-test
-python3 microbenchmark.py --n-to-m --functional-test
+python3 microbenchmark.py --suite=1-to-m --functional-test
+python3 microbenchmark.py --suite=n-to-m --functional-test
 python3 benchmark.py --suite=unet --comm-overlap-level 0 --exp-name unet.baseline
 python3 benchmark.py --suite=unet --comm-overlap-level 1 --exp-name unet.overlap
 python3 benchmark.py --suite=unet --comm-overlap-level 2 --exp-name unet.overlap_with_schedule
